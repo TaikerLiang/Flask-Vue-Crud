8efcad3795ec3f7152bfe9a36af0fddda0f860c5
TaikerLiang <liang0816tw@gmail.com>
2019-07-06 16:54:57 +0800

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  margin-top: 60px
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
