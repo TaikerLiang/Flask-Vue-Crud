e9a6d5e0711059e5e62be7a4ebdc3a5c1c8122ac
TaikerLiang <liang0816tw@gmail.com>
2019-07-07 21:27:42 +0800

    add test & refactor code

--- server/run.py
+++ server/run.py
@@ -1,87 +1,27 @@
+"""
+.. module:: run
+   :platform: Ubuntu 18.04 & Mac OS
+   :synopsis: Main function of flask service
+
+.. moduleauthor:: Paul Liang <liang0816tw@gmail.com>
+.. date:: 2019-07-07
+"""
+
 from flask import Flask, jsonify, request
-from flask_cors import CORS
-import uuid
+from src import app
+import os
 
-BOOKS = [
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
 
-# configuration
-DEBUG = True
-
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
+@app.cli.command()
+def test():
+    os.system("pytest -x")
 
 
 # sanity check route
-@app.route('/ping', methods=['GET'])
+@app.route("/ping", methods=["GET"])
 def ping_pong():
-    return jsonify('pong!')
+    return jsonify("pong!")
 
 
-@app.route('/books', methods=['GET', 'POST'])
-def all_books():
-    response_object = {'status': 'success'}
-    if request.method == 'POST':
-        post_data = request.get_json()
-        BOOKS.append({
-            'id': uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read')
-        })
-        response_object['message'] = 'Book added!'
-    else:
-        response_object['books'] = BOOKS
-    return jsonify(response_object)
-
-
-@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
-def single_book(book_id):
-    response_object = {'status': 'success'}
-    if request.method == 'PUT':
-        post_data = request.get_json()
-        remove_book(book_id)
-        BOOKS.append({
-            'id': uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read')
-        })
-        response_object['message'] = 'Book updated!'
-    if request.method == 'DELETE':
-        remove_book(book_id)
-        response_object['message'] = 'Book removed!'
-    return jsonify(response_object)
-
-def remove_book(book_id):
-    for book in BOOKS:
-        if book['id'] == book_id:
-            BOOKS.remove(book)
-            app.logger.error(BOOKS)
-            return True
-    return False
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
