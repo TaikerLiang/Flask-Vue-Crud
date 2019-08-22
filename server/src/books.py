99d56766d797d669a8410b2c229666233395f47e
TaikerLiang <liang0816tw@gmail.com>
2019-08-22 17:24:44 +0800

    udpate

--- server/src/books.py
+++ server/src/books.py
@@ -18,27 +18,6 @@ import uuid
 
 api = Api(app)
 
-BOOKS = [
-    {
-        "id": uuid.uuid4().hex,
-        "title": "On the Road",
-        "author": "Jack Kerouac",
-        "read": True,
-    },
-    {
-        "id": uuid.uuid4().hex,
-        "title": "Harry Potter and the Philosopher's Stone",
-        "author": "J. K. Rowling",
-        "read": False,
-    },
-    {
-        "id": uuid.uuid4().hex,
-        "title": "Green Eggs and Ham",
-        "author": "Dr. Seuss",
-        "read": True,
-    },
-]
-
 
 class RBooks(Resource):
     """
