8d186b184e056352b2ec3ff37cf067a940826c0f
Yoyo <b850108cd@gmail.com>
2019-08-22 18:24:53 +0800

    (Carrier)(COSU) modify run_spider
    add setting to run_spider

--- test/spiders/carrier_cosu/run_spider.py
+++ test/spiders/carrier_cosu/run_spider.py
@@ -1,15 +1,22 @@
+import os
+
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils import project
 
 from crawler.spiders.carrier_cosu import CarrierCosuSpider
 
 
 if __name__ == '__main__':
-    process = CrawlerProcess()
+    os.environ[project.ENVVAR] = 'crawler.settings'
+    settings = project.get_project_settings()
+
+    process = CrawlerProcess(settings=settings)
 
     mbl_nos = [
         # 6205506031,
-        6210544990, 6213846630, 6085396930, 6199589860,
-        # 6201756510, 6201756520, 6202096890, 6204349640, 6204349820,
+        # 6210544990, 6213846630, 6085396930, 6199589860,
+        6201756510,
+        # 6201756520, 6202096890, 6204349640, 6204349820,
         # 6205526740, 6205547483, 6210544910, 6210930180, 6210930190,
         # 6212347340, 6212511950, 6213698500, 6213940280, 6214337410,
         # 6214501030, 6215813130, 6215813140, 6215813150, 6215813160,
