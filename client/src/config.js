0fc269084dd8bde1df65456d0855752ac0641ced
TaikerLiang <liang0816tw@gmail.com>
2019-08-28 13:13:57 +0800

    add certbot service

--- client/src/config.js
+++ client/src/config.js
@@ -6,17 +6,11 @@ if (process.env.VUE_APP_ENV === "dev") {
   config = {
     $api_url: "http://localhost",
     timeoutDuration: 30000,
-    devServer: {
-        disableHostCheck: true
-    }
   };
 } else {
   config = {
     $api_url: "https://api.taiker.space",
     timeoutDuration: 1000,
-    devServer: {
-        disableHostCheck: true
-    }
   };
 }
 
