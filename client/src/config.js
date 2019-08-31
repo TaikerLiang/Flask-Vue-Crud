4526ccbd4162cdf82c209360a8771c9c3625e403
TaikerLiang <liang0816tw@gmail.com>
2019-08-31 16:18:45 +0800

    Https (#5)
    * add certbot service

--- client/src/config.js
+++ client/src/config.js
@@ -4,12 +4,12 @@ console.log(process.env);
 
 if (process.env.VUE_APP_ENV === "dev") {
   config = {
-    $api_url: "http://localhost",
+    $api_url: "http://localhost:5000",
     timeoutDuration: 30000,
   };
 } else {
   config = {
-    $api_url: "http://54.248.53.18",
+    $api_url: "https://api.taiker.space",
     timeoutDuration: 1000,
   };
 }
