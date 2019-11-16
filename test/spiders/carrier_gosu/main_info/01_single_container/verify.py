5df2cf8c56759a467622ea73d36c44b07e282831
汪子惟 <m3iloveyoum3@gmail.com>
2019-11-16 13:49:35 +0800

    (Carrier)(GOSU) add test

---
+++ test/spiders/carrier_gosu/main_info/01_single_container/verify.py
@@ -0,0 +1,24 @@
+import scrapy
+
+from crawler.core_carrier.items import MblItem, ContainerItem, LocationItem
+
+
+def verify(results):
+    assert results[0] == MblItem(
+        mbl_no='GOSUNGB9490855',
+        por=LocationItem(name=None),
+        pol=LocationItem(name="Ningbo , China. People's Republic"),
+        pod=LocationItem(name="Laem Chabang, Thailand"),
+        final_dest=LocationItem(name=None),
+        etd='27-Sep-2019',
+        atd='06-Oct-2019',
+        vessel='Cape Flint',
+        voyage='29',
+    )
+
+    assert results[1] == ContainerItem(
+        container_key='ZCSU2764374',
+        container_no='ZCSU2764374',
+    )
+
+    assert isinstance(results[2], scrapy.Request)
