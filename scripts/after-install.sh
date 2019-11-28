e0da416ecdcca05aa80fb11359cd0fb2d1d4fcb6
TaikerLiang <liang0816tw@gmail.com>
2019-11-28 19:00:19 +0800

    update ci config & docker config

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/flask-vue-crud
-echo "DOCKER_REGISTRY=364135942435.dkr.ecr.ap-northeast-1.amazonaws.com/xinyisheng-erp" >> .env
+echo "DOCKER_REGISTRY=979007176950.dkr.ecr.ap-northeast-1.amazonaws.com/flask-vue-crud" >> .env
 echo "BRANCH="$DEPLOYMENT_GROUP_NAME >> .env
