6325d23d8f3e9886d587bd2f8f617d63f17c803a
TaikerLiang <liang0816tw@gmail.com>
2019-11-29 20:49:53 +0800

    update ci config & docker config

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,4 +2,3 @@ eval $(aws ecr get-login --no-include-email --region ap-northeast-1)
 cd /home/ubuntu/flask-vue-crud
 docker-compose pull
 docker-compose up -d
-docker-compose exec -e FLASK_APP=run.py server flask test
