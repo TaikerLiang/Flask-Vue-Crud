6a8c3be36285f2db3201090f619d574ee0372a96
TaikerLiang <liang0816tw@gmail.com>
2019-11-29 21:36:53 +0800

    update ci config & docker config

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chown -R ubuntu:root /home/ubuntu/flask-vue-crud
 cd /home/ubuntu/flask-vue-crud
 echo "DOCKER_REGISTRY=979007176950.dkr.ecr.ap-northeast-1.amazonaws.com/flask-vue-crud" >> .env
 echo "BRANCH="$DEPLOYMENT_GROUP_NAME >> .env
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo chown -R ubuntu:root /home/ubuntu/flask-vue-crud
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
