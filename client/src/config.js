a65b014a3fec48c2cb209ba692ed11c92410ded0
TaikerLiang <liang0816tw@gmail.com>
2019-12-04 22:28:51 +0800

    update ci config & docker config

--- client/src/config.js
+++ client/src/config.js
@@ -9,7 +9,7 @@ if (process.env.VUE_APP_ENV === "dev") {
   };
 } else {
   config = {
-    $api_url: "https://api.taiker.cc",
+    $api_url: "https://api.taiker.net",
     timeoutDuration: 1000,
   };
 }
