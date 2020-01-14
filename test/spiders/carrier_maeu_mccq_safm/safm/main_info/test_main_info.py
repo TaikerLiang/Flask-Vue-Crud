054ba4777feda2dcc3e2a3d8dcf8c53bcbca20a0
汪子惟 <m3iloveyoum3@gmail.com>
2020-01-14 17:19:58 +0800

    (Carrier)(MAEU)(MCCQ)(SAFM) modify test

--- test/spiders/carrier_maeu_mccq_safm/safm/main_info/test_main_info.py
+++ test/spiders/carrier_maeu_mccq_safm/safm/main_info/test_main_info.py
@@ -5,7 +5,6 @@ from scrapy import Request
 from scrapy.http import TextResponse
 
 from crawler.core_carrier.exceptions import CarrierInvalidMblNoError
-from crawler.core_carrier.rules import RuleManager
 from crawler.spiders.carrier_maeu_mccq_safm import MainInfoRoutingRule, CarrierSafmSpider
 from test.spiders.carrier_maeu_mccq_safm.safm import main_info
 from test.spiders.utils import extract_url_from
@@ -35,14 +34,11 @@ def test_main_info_routing_rule(sub, mbl_no, sample_loader):
         encoding='utf-8',
         request=Request(
             url=url,
-            meta={
-                RuleManager.META_CARRIER_CORE_RULE_NAME: MainInfoRoutingRule.name,
-            }
         )
     )
 
-    spider = CarrierSafmSpider(mbl_no=mbl_no)
-    results = list(spider.parse(response=response))
+    routing_rule = MainInfoRoutingRule()
+    results = list(routing_rule.handle(response=response))
 
     verify_module = sample_loader.load_sample_module(sub, 'verify')
     verify_module.verify(results=results)
@@ -64,12 +60,10 @@ def test_main_info_handler_mbl_no_error(sub, mbl_no, expect_exception, sample_lo
         encoding='utf-8',
         request=Request(
             url=url,
-            meta={
-                RuleManager.META_CARRIER_CORE_RULE_NAME: MainInfoRoutingRule.name,
-            }
         )
     )
 
-    spider = CarrierSafmSpider(mbl_no=mbl_no)
+    routing_rule = MainInfoRoutingRule()
+
     with pytest.raises(expect_exception):
-        list(spider.parse(response=response))
+        list(routing_rule.handle(response=response))
