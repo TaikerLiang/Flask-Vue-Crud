21eb60cf4fc93441007236bd2ddccd6e0df740d0
汪子惟 <m3iloveyoum3@gmail.com>
2020-06-08 17:45:01 +0800

    (Vessel)(Big Schedule) modify test

--- test/spiders/vessel_big_schedules/port_info/test_vessel_schedule_routing_rule.py
+++ test/spiders/vessel_big_schedules/port_info/test_vessel_schedule_routing_rule.py
@@ -4,7 +4,6 @@ import pytest
 from scrapy.http import TextResponse
 
 from crawler.spiders.vessel_big_schedules import VesselScheduleRoutingRule
-from test.spiders.utils import extract_url_from
 from test.spiders.vessel_big_schedules import port_info
 
 
@@ -22,12 +21,11 @@ def test_vessel_schedule_routing_rule(sub, scac, vessel_name, carrier_id, vessel
     json_text = sample_loader.read_file(sub, 'sample.json')
 
     cookie = {}
-    routing_request = VesselScheduleRoutingRule.build_routing_request(
+    option = VesselScheduleRoutingRule.build_request_option(
         scac=scac, vessel_name=vessel_name, carrier_id=carrier_id, vessel_gid=vessel_gid, cookie=cookie)
-    url = extract_url_from(routing_request=routing_request)
 
     response = TextResponse(
-        url=url,
+        url=option.url,
         encoding='utf-8',
         body=json_text,
         headers={
