9b642ab58d7928a65c9ae7427b99d73b6866fcf8
Paul Liang <paul.liang@hardcoretech.co>
2021-04-22 22:47:13 +0800

    fix(tracking_terminal): fix the lower_short of piera terminal for correct url

--- src/crawler/spiders/terminal_pier_multi.py
+++ src/crawler/spiders/terminal_pier_multi.py
@@ -4,7 +4,7 @@ from crawler.core_terminal.tideworks_share_spider import TideworksShareSpider, C
 class TerminalPierSpider(TideworksShareSpider):
     name = 'terminal_pier_multi'
     company_info = CompanyInfo(
-        lower_short='pier',
+        lower_short='piera',
         upper_short='PA',
         email='w87818@yahoo.com.tw',
         password='1234567890',
