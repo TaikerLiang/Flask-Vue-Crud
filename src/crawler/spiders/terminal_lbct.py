6c193fa5cc16f24d0083bb0c922c24550291d7cc
Paul Liang <paul.liang@hardcoretech.co>
2021-04-27 12:44:10 +0800

    feat(tracking_termainal): complete the lbct terminal

--- src/crawler/spiders/terminal_lbct.py
+++ src/crawler/spiders/terminal_lbct.py
@@ -1,12 +1,12 @@
 import json
 import re
 import time
-from typing import Dict
+from typing import Dict, List
 
 import scrapy
 
-from crawler.core_terminal.base_spiders import BaseTerminalSpider
-from crawler.core_terminal.items import DebugItem, BaseTerminalItem, TerminalItem
+from crawler.core_terminal.base_spiders import BaseMultiTerminalSpider
+from crawler.core_terminal.items import DebugItem, TerminalItem, InvalidContainerNoItem
 from crawler.core_terminal.request_helpers import RequestOption
 from crawler.core_terminal.rules import RuleManager, BaseRoutingRule
 
@@ -14,7 +14,7 @@ from crawler.core_terminal.rules import RuleManager, BaseRoutingRule
 BASE_URL = 'https://www.lbct.com'
 
 
-class TerminalLbctSpider(BaseTerminalSpider):
+class TerminalLbctSpider(BaseMultiTerminalSpider):
     name = 'terminal_lbct'
 
     def __init__(self, *args, **kwargs):
@@ -27,7 +27,8 @@ class TerminalLbctSpider(BaseTerminalSpider):
         self._rule_manager = RuleManager(rules=rules)
 
     def start(self):
-        option = ContainerRoutingRule.build_request_option(container_no=self.container_no)
+        unique_container_nos = list(self.cno_tid_map.keys())
+        option = ContainerRoutingRule.build_request_option(container_no_list=unique_container_nos)
         yield self._build_request_by(option=option)
 
     def parse(self, response):
@@ -39,8 +40,12 @@ class TerminalLbctSpider(BaseTerminalSpider):
         self._saver.save(to=save_name, text=response.text)
 
         for result in routing_rule.handle(response=response):
-            if isinstance(result, BaseTerminalItem):
-                yield result
+            if isinstance(result, TerminalItem) or isinstance(result, InvalidContainerNoItem):
+                c_no = result['container_no']
+                t_ids = self.cno_tid_map[c_no]
+                for t_id in t_ids:
+                    result['task_id'] = t_id
+                    yield result
             elif isinstance(result, RequestOption):
                 yield self._build_request_by(option=result)
             else:
@@ -69,83 +74,90 @@ class ContainerRoutingRule(BaseRoutingRule):
     name = 'CONTAINER'
 
     @classmethod
-    def build_request_option(cls, container_no) -> RequestOption:
+    def build_request_option(cls, container_no_list: List) -> RequestOption:
         timestamp = cls._build_timestamp()
-        url = f'{BASE_URL}/CargoSearch/GetMultiCargoSearchJson?timestamp={timestamp}&listOfSearchId={container_no}'
+        url = f'{BASE_URL}/CargoSearch/GetMultiCargoSearchJson?timestamp={timestamp}&listOfSearchId={",".join(container_no_list)}'
 
         return RequestOption(
             rule_name=cls.name,
             method=RequestOption.METHOD_GET,
             url=url,
+            meta={
+                'container_no_list': container_no_list
+            }
         )
 
     def get_save_name(self, response) -> str:
         return f'{self.name}.json'
 
     def handle(self, response):
+        container_no_list = response.meta['container_no_list']
         response_dict = json.loads(response.text)
 
-        container_info = self._extract_container_info(response_dict=response_dict)
+        for response in response_dict:
+            if not response['containerId'] in container_no_list:
+                continue
 
-        yield TerminalItem(
-            container_no=container_info['container_no'],
-            freight_release=container_info['freight_hold'],
-            customs_release=container_info['customs_hold'],
-            discharge_date=container_info['discharge_date'],
-            ready_for_pick_up=container_info['ready_for_pick_up'],
-            appointment_date=container_info['appointment_date'],
-            last_free_day=container_info['last_free_day'],
-            carrier=container_info['carrier'],
-            container_spec=container_info['type'],
-            cy_location=container_info['location'],
-            vessel=container_info['vessel'],
-            voyage=container_info['voyage'],
+            container_info = self._extract_container_info(response=response)
 
-            # on html
-            # field same like other terminal
-            tmf=container_info['tmf_hold'],
+            yield TerminalItem(
+                container_no=container_info['container_no'],
+                freight_release=container_info['freight_hold'],
+                customs_release=container_info['customs_hold'],
+                discharge_date=container_info['discharge_date'],
+                ready_for_pick_up=container_info['ready_for_pick_up'],
+                appointment_date=container_info['appointment_date'],
+                gate_out_date=container_info['gate_out_date'],
+                last_free_day=container_info['last_free_day'],
+                carrier=container_info['carrier'],
+                container_spec=container_info['type'],
+                cy_location=container_info['location'],
+                vessel=container_info['vessel'],
+                voyage=container_info['voyage'],
 
-            # new field
-            owed=container_info['owed'],
-            full_empty=container_info['full/empty'],
-        )
+                # on html
+                # field same like other terminal
+                tmf=container_info['tmf_hold'],
+
+                # new field
+                owed=container_info['owed'],
+                full_empty=container_info['full/empty'],
+            )
 
     @staticmethod
-    def _extract_container_info(response_dict: Dict) -> Dict:
+    def _extract_container_info(response: Dict) -> Dict:
         # pattern = re.compile(r'^(?P<discharge_date>\d{2}/\d{2}/\d{4})')
-
-        first_container = response_dict[0]
-
         appt_date_time = None
-        if first_container.get('fakeId'):
-            raw_appt_date_time = first_container['fakeId']
+        if response.get('fakeId'):
+            raw_appt_date_time = response['fakeId']
             appt_date_time = raw_appt_date_time.split('#')[0]
 
         tmf, customs, freight = None, None, None
-        if first_container.get('listOfFlag'):
-            tmf = first_container['listOfFlag'][0]['type']
-            customs = first_container['listOfFlag'][1]['type']
-            freight = first_container['listOfFlag'][2]['type']
+        if response.get('listOfFlag'):
+            tmf = response['listOfFlag'][0]['type']
+            customs = response['listOfFlag'][1]['type']
+            freight = response['listOfFlag'][2]['type']
 
         # m = pattern.match(container['discharged'])
         # discharge_date = m.group('discharge_date')
 
         return {
-            'container_no': first_container['containerId'],
-            'discharge_date': first_container['discharged'],
-            'type': first_container['type'],
-            'ready_for_pick_up': first_container['available'],
-            'last_free_day': first_container['freeTimeExpiration'],
+            'container_no': response['containerId'],
+            'discharge_date': response['discharged'],
+            'type': response['type'],
+            'ready_for_pick_up': response['available'],
+            'last_free_day': response['freeTimeExpiration'],
             'appointment_date': appt_date_time,
-            'vessel': first_container['vessel'],
-            'voyage': first_container['inboundVoyageNumber'],
-            'carrier': first_container['line'],
-            'location': first_container['location'],
-            'full/empty': first_container['freightKind'],
+            'gate_out_date': response['dateDeliverd'],
+            'vessel': response['vessel'],
+            'voyage': response['inboundVoyageNumber'],
+            'carrier': response['line'],
+            'location': response['location'],
+            'full/empty': response['freightKind'],
             'tmf_hold': tmf,
             'customs_hold': customs,
             'freight_hold': freight,
-            'owed': first_container['owed'],
+            'owed': response['owed'],
         }
 
     @staticmethod
