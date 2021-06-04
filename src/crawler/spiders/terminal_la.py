73a63d2d93b2b4da7df5232c671f9017f3370b90
Paul Liang <paul.liang@hardcoretech.co>
2021-06-04 16:15:04 +0800

    fix(tracking_terminal): complete all apm terminal in USA

--- src/crawler/spiders/terminal_la.py
+++
@@ -1,7 +0,0 @@
-from crawler.core_terminal.apm_share_spider import ApmShareSpider
-
-
-class TerminalApmLASpider(ApmShareSpider):
-    firms_code = 'W185'
-    name = 'terminal_apm_la_multi'
-    terminal_id = 'c56ab48b-586f-4fd2-9a1f-06721c94f3bb'
