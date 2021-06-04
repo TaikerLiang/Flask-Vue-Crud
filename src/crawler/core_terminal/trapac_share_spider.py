13dcf1d71eadcb15da5ef7aea056ff096b379a26
Paul Liang <paul.liang@hardcoretech.co>
2021-06-04 15:12:44 +0800

    feat(tracking_terminal): add trapac terminals

--- src/crawler/core_terminal/trapac_share_spider.py
+++ src/crawler/core_terminal/trapac_share_spider.py
@@ -5,10 +5,12 @@ import string
 from typing import List, Dict
 from datetime import datetime, timedelta
 from urllib.parse import urlencode
+import ujson as json
 
 
 import scrapy
 from scrapy import Selector
+
 # from selenium import webdriver
 from seleniumwire import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -18,7 +20,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from anticaptchaofficial.recaptchav2proxyless import *
 from selenium.common.exceptions import NoSuchElementException
 
-from crawler.core_carrier.exceptions import LoadWebsiteTimeOutError
+from crawler.core_carrier.exceptions import LoadWebsiteTimeOutError, DataNotFoundError
 from crawler.core_terminal.base_spiders import BaseMultiTerminalSpider
 from crawler.core_terminal.items import DebugItem, TerminalItem, InvalidContainerNoItem
 from crawler.core_terminal.request_helpers import RequestOption
@@ -33,6 +35,7 @@ class CompanyInfo:
     upper_short: str
     email: str
     password: str
+
 
 @dataclasses.dataclass
 class ProxyOption:
@@ -77,17 +80,16 @@ class TrapacShareSpider(BaseMultiTerminalSpider):
     def __init__(self, *args, **kwargs):
         super(TrapacShareSpider, self).__init__(*args, **kwargs)
 
-        rules = [
-            MainRoutingRule(),
-            ContentRoutingRule()
-        ]
+        rules = [MainRoutingRule(), ContentRoutingRule()]
 
         self._rule_manager = RuleManager(rules=rules)
         self._save = True if 'save' in kwargs else False
 
     def start(self):
         unique_container_nos = list(self.cno_tid_map.keys())
-        option = MainRoutingRule.build_request_option(container_no_list=unique_container_nos, company_info=self.company_info)
+        option = MainRoutingRule.build_request_option(
+            container_no_list=unique_container_nos, company_info=self.company_info
+        )
         yield self._build_request_by(option=option)
 
     def parse(self, response):
@@ -107,7 +109,7 @@ class TrapacShareSpider(BaseMultiTerminalSpider):
             elif isinstance(result, SaveItem) and self._save:
                 self._saver.save(to=result.file_name, text=result.text)
             elif isinstance(result, SaveItem) and not self._save:
-                pass
+                raise DataNotFoundError
 
     def _build_request_by(self, option: RequestOption):
         meta = {
@@ -161,9 +163,7 @@ class MainRoutingRule(BaseRoutingRule):
 
         is_g_captcha, res, cookies = self._build_container_response(company_info=company_info, container_no_list=container_no_list)
         if is_g_captcha:
-            if res:
-                print('print(is_g_captcha, res, cookies)', is_g_captcha, res, cookies)
-                yield ContentRoutingRule.build_request_option(container_no_list, company_info=company_info, g_token=res, cookies=cookies)
+            yield ContentRoutingRule.build_request_option(container_no_list=container_no_list, company_info=company_info, g_token=res, cookies=cookies)
         else:
             container_response = scrapy.Selector(text=res)
             yield SaveItem(file_name='container.html', text=container_response.get())
@@ -180,75 +180,6 @@ class MainRoutingRule(BaseRoutingRule):
                     vessel=container_info['vessel'],  # vsl / voy
                     voyage=container_info['voyage'],  # vsl / voy
                 )
-
-    @staticmethod
-    def _build_container_response(company_info: CompanyInfo, container_no_list: List):
-        content_getter = ContentGetter(company_info=company_info)
-        is_g_captcha, res, cookies = content_getter.get_content(search_no=','.join(container_no_list))
-        content_getter.quit()
-
-        return is_g_captcha, res, cookies
-
-    @staticmethod
-    def _is_search_no_invalid(response: scrapy.Selector) -> bool:
-        return bool(response.css('tr.error-row'))
-
-
-class ContentRoutingRule(BaseRoutingRule):
-    name = 'CONTENT'
-
-    @classmethod
-    def build_request_option(cls, container_no_list: Dict, company_info: CompanyInfo, g_token: str, cookies: Dict) -> RequestOption:
-        form_data = {
-            'action': 'trapac_transaction',
-            'recaptcha-token': g_token,
-            'terminal': company_info.upper_short,
-            'transaction': 'availability',
-            'containers': ','.join(container_no_list),
-            'container': '',
-            'booking': '',
-            'email': '',
-            'equipment_type': 'CT',
-            'history_type': 'N',
-            'services': '',
-            'from_date': str(datetime.now().date()),
-            'to_date': str((datetime.now() + timedelta(days=30)).date())
-        }
-
-        headers = {
-            'authority': f'{company_info.lower_short}.trapac.com',
-            'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-            'accept': 'application/json, text/javascript, */*; q=0.01',
-            'x-requested-with': 'XMLHttpRequest',
-            'sec-ch-ua-mobile': '?0',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'origin': f'https://{company_info.lower_short}.trapac.com',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': f'https://{company_info.lower_short}.trapac.com/quick-check/?terminal=',
-            'accept-language': 'en-US,en;q=0.9',
-            'cookie': CookieHelper.get_cookie_str(cookies),
-        }
-
-        return RequestOption(
-            rule_name=cls.name,
-            method=RequestOption.METHOD_POST_BODY,
-            url=f'https://{company_info.lower_short}.trapac.com/wp-admin/admin-ajax.php',
-            headers=headers,
-            body=urlencode(query=form_data),
-            meta={
-                'numbers': len(container_no_list),
-            },
-        )
-
-    def handle(self, response):
-        numbers = response.meta['numbers']
-
-        print('paul response.body', numbers, response.body)
-        self._extract_container_result_table(response, numbers)
-        yield
 
     @staticmethod
     def _extract_container_result_table(response: scrapy.Selector, numbers: int):
@@ -273,6 +204,97 @@ class ContentRoutingRule(BaseRoutingRule):
                 'vessel': vessel,
                 'voyage': voyage,
             }
+
+    @staticmethod
+    def _build_container_response(company_info: CompanyInfo, container_no_list: List):
+        content_getter = ContentGetter(company_info=company_info)
+        is_g_captcha, res, cookies = content_getter.get_content(search_no=','.join(container_no_list))
+        content_getter.quit()
+
+        return is_g_captcha, res, cookies
+
+    @staticmethod
+    def _is_search_no_invalid(response: scrapy.Selector) -> bool:
+        return bool(response.css('tr.error-row'))
+
+
+class ContentRoutingRule(BaseRoutingRule):
+    name = 'CONTENT'
+
+    @classmethod
+    def build_request_option(
+        cls, container_no_list: List, company_info: CompanyInfo, g_token: str, cookies: Dict
+    ) -> RequestOption:
+        form_data = {
+            'action': 'trapac_transaction',
+            'recaptcha-token': g_token,
+            'terminal': company_info.upper_short,
+            'transaction': 'availability',
+            'containers': ','.join(container_no_list),
+            'booking': '',
+            'email': '',
+            'equipment_type': 'CT',
+            'history_type': 'N',
+            'services': '',
+            'from_date': str(datetime.now().date()),
+            'to_date': str((datetime.now() + timedelta(days=30)).date()),
+        }
+
+        headers = {
+            'authority': f'{company_info.lower_short}.trapac.com',
+            'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'x-requested-with': 'XMLHttpRequest',
+            'sec-ch-ua-mobile': '?0',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'origin': f'https://{company_info.lower_short}.trapac.com',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'referer': f'https://{company_info.lower_short}.trapac.com/quick-check/?terminal={company_info.upper_short}&transaction=availability',
+            'accept-language': 'en-US,en;q=0.9',
+            'cookie': CookieHelper.get_cookie_str(cookies),
+        }
+
+        return RequestOption(
+            rule_name=cls.name,
+            method=RequestOption.METHOD_POST_BODY,
+            url=f'https://{company_info.lower_short}.trapac.com/wp-admin/admin-ajax.php',
+            headers=headers,
+            body=urlencode(query=form_data),
+            meta={
+                'numbers': len(container_no_list),
+            },
+        )
+
+    def handle(self, response):
+        numbers = response.meta['numbers']
+        resp = json.loads(response.text)
+
+        if 'Please complete the reCAPTCHA check and submit your request again' in resp['html']:
+            raise DataNotFoundError
+
+        resp_html = Selector(text=resp['html'])
+        table = resp_html.css('div[class="transaction-result availability"] table')
+        table_locator = ContainerTableLocator()
+        table_locator.parse(table=table, numbers=numbers)
+        table_extractor = TableExtractor(table_locator=table_locator)
+
+        vessel, voyage = table_extractor.extract_cell(top='Vsl / Voy', left=0, extractor=VesselVoyageTdExtractor())
+
+        for i in range(numbers):
+            yield TerminalItem(
+                container_no=table_extractor.extract_cell(top='Number', left=i),
+                customs_release=table_extractor.extract_cell(top='Holds_Customs', left=i),
+                cy_location=table_extractor.extract_cell(top='Yard Status', left=i),
+                last_free_day=table_extractor.extract_cell(top='Demurrage_LFD', left=i),
+                holds=table_extractor.extract_cell(top='Demurrage_Hold', left=i),
+                demurrage=table_extractor.extract_cell(top='Demurrage_Amt', left=i),
+                container_spec=table_extractor.extract_cell(top='Dimensions', left=i),
+                vessel=vessel,
+                voyage=voyage,
+            )
 
 # ------------------------------------------------------------------------
 
@@ -300,19 +322,18 @@ class HeadlessBrowser:
         options.add_experimental_option('excludeSwitches', ['enable-automation'])
         options.add_experimental_option('useAutomationExtension', False)
 
-        # PROXY_GROUP_SHADER = 'SHADER'
-        # PROXY_GROUP_RESIDENTIAL = 'RESIDENTIAL'
-        # proxy_option = ProxyOption(group=PROXY_GROUP_SHADER, session=f'trapac{self._generate_random_string()}')
-        # seleniumwire_options = {
-        #     'connection_timeout': None,
-        #     'proxy': {
-        #         'http': f'http://{self.get_proxy_username(proxy_option)}:{self.PROXY_PASSWORD}@{self.PROXY_URL}',
-        #         'https': f'https://{self.get_proxy_username(proxy_option)}:{self.PROXY_PASSWORD}@{self.PROXY_URL}',
-        #         'no_proxy': 'localhost,127.0.0.1'
-        #     }
-        # }
-        # self._browser = webdriver.Chrome(chrome_options=options, seleniumwire_options=seleniumwire_options)
-        self._browser = webdriver.Chrome(chrome_options=options)
+        PROXY_GROUP_RESIDENTIAL = 'RESIDENTIAL'
+        proxy_option = ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'trapac{self._generate_random_string()}')
+        seleniumwire_options = {
+            'connection_timeout': None,
+            'proxy': {
+                'http': f'http://{self.get_proxy_username(proxy_option)}:{self.PROXY_PASSWORD}@{self.PROXY_URL}',
+                'https': f'https://{self.get_proxy_username(proxy_option)}:{self.PROXY_PASSWORD}@{self.PROXY_URL}',
+                'no_proxy': 'localhost,127.0.0.1',
+            },
+        }
+        self._browser = webdriver.Chrome(chrome_options=options, seleniumwire_options=seleniumwire_options)
+        # self._browser = webdriver.Chrome(chrome_options=options)
 
     def get(self, url):
         self._browser.get(url=url)
@@ -356,6 +377,9 @@ class HeadlessBrowser:
 
     def save_screenshot(self):
         self._browser.save_screenshot("screenshot.png")
+
+    def get_g_token(self):
+        return self._browser.find_element_by_xpath('//*[@id="transaction-form"]/input').get_attribute('value')
 
     def get_google_recaptcha(self):
         try:
@@ -404,7 +428,7 @@ class ContentGetter:
 
     def find_ip(self):
         self._headless_browser.get('https://www.whatismyip.com.tw/')
-        time.sleep(3)
+        time.sleep(5)
 
         ip_selector = self._headless_browser.find_element_by_css_selector('b span')
         print('find_id', ip_selector.text)
@@ -421,14 +445,15 @@ class ContentGetter:
         )
         self._headless_browser.accept_cookie()
         self._headless_browser.key_in_search_bar(search_no=search_no)
-        self._headless_browser.press_search_button()
         cookies = self._headless_browser.get_cookies()
+
+        g_response = self._headless_browser.solve_google_recaptcha(self._company.lower_short)
+        print('get_g_token', g_response)
+        self._headless_browser.press_search_button()
+
         if self._headless_browser.get_google_recaptcha():
             g_response = self._headless_browser.solve_google_recaptcha(self._company.lower_short)
             return True, g_response, cookies
-
-        time.sleep(20)
-        self._headless_browser.save_screenshot()
 
         return False, self.get_result_response_text(), cookies
 
