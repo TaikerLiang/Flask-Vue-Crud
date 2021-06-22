cab0136b54bdc41c47e8c7f4490133d7043bacc4
Paul Liang <paul.liang@hardcoretech.co>
2021-06-23 00:29:29 +0800

    feat(internal): add the scrpit to control the deployment, there are some sensitive information in Dockerfile.*, so we ignore them.

---
+++ src/deploy.sh
@@ -0,0 +1,28 @@
+# -t: options, prd/eval/dev
+# ex: sh deploy -t prd
+
+while getopts t: flag
+do
+    case "${flag}" in
+        t) target=${OPTARG};;
+    esac
+done
+
+if [ "x$target" = "xprd" ]; then
+    echo "deploy to scrapy cloud project: 407697"
+    shub image build 407697 -f Dockerfile.prd
+    shub image push 407697
+    shub image deploy 407697
+elif [ "x$target" = "xeval" ]; then
+    echo "deploy to scrapy cloud project: 407696"
+    shub image build 407696 -f Dockerfile.eval
+    shub image push 407696
+    shub image deploy 407696
+elif [ "x$target" = "xdev" ]; then
+    echo "deploy to scrapy cloud project: 448553"
+    shub image build 448553 -f Dockerfile.dev
+    shub image push 448553
+    shub image deploy 448553
+else
+    echo "invalid -t paramter, one of prd/eval/dev only"
+fi
