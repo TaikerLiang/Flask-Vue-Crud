942525da498b2e4835aa8457714aeb21ecf7bd58
Paul Liang <paul.liang@hardcoretech.co>
2021-07-12 02:35:15 +0800

    fix(tracking_carrier): remove the duplicated spiders

--- src/crawler/spiders/carrier_maeu.py
+++
@@ -1,5 +0,0 @@
-from crawler.core_carrier.maeu_mccq_safm_share_spider import MaeuMccqSafmShareSpider
-
-class CarrierMaeuSpider(MaeuMccqSafmShareSpider):
-    name = 'carrier_maeu'
-    base_url_format = 'https://api.maerskline.com/track/{search_no}'
