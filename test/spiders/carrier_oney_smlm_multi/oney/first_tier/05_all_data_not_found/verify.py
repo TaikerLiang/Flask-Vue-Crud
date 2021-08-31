ec83225545465f0b91e9321e218f3cbdfa8ef899
Paul Liang <paul.liang@hardcoretech.co>
2021-08-31 11:29:46 +0800

    test(tracking_carrier): fix test error

--- test/spiders/carrier_oney_smlm_multi/oney/first_tier/05_all_data_not_found/verify.py
+++ test/spiders/carrier_oney_smlm_multi/oney/first_tier/05_all_data_not_found/verify.py
@@ -3,4 +3,4 @@ from crawler.core_carrier.oney_smlm_multi_share_spider import Restart
 
 
 def verify(results):
-    assert results[0] == Restart(reason='IP block', search_no=['DALA35925000', 'OB-21060019'], task_id=[1, 2])
+    assert results[0] == Restart(reason='IP block', search_nos=['DALA35925000', 'OB-21060019'], task_ids=[1, 2])
--- test/spiders/carrier_oney_smlm_multi/smlm/first_tier/04_data_not_found/verify.py
+++ test/spiders/carrier_oney_smlm_multi/smlm/first_tier/04_data_not_found/verify.py
@@ -2,4 +2,4 @@ from crawler.core_carrier.oney_smlm_multi_share_spider import Restart
 
 
 def verify(results):
-    assert results[0] == Restart(reason='IP block', search_no=['SHSB1FY71701'], task_id=[1])
+    assert results[0] == Restart(reason='IP block', search_nos=['SHSB1FY71701'], task_ids=[1])
