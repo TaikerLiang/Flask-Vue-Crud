d620a03f5a1c0a80e1dd7588875423a98c8b7a4c
FloraTesng <flora.tseng@gofreight.co>
2021-09-08 16:48:08 +0800

    test(carrier_mscu_multi): add test

---
+++ test/spiders/carrier_mscu_multi/main_info/03_multi_containers/verify.py
@@ -0,0 +1,54 @@
+from crawler.core_carrier.items import MblItem, ContainerItem, ContainerStatusItem, LocationItem
+
+
+def verify(results):
+    assert results[0] == ContainerItem(
+        container_key='GLDU7636572',
+        container_no='GLDU7636572',
+        task_id='1',
+    )
+
+    assert results[1] == ContainerStatusItem(
+        container_key='GLDU7636572',
+        description='Empty in Container Yard',
+        local_date_time='23/10/2019',
+        location=LocationItem(name='LONG BEACH, CA, US'),
+        vessel=None,
+        voyage=None,
+        est_or_actual='A',
+        task_id='1',
+    )
+
+    assert results[11] == ContainerItem(
+        container_key='MEDU4550625',
+        container_no='MEDU4550625',
+        task_id='1',
+    )
+
+    assert results[12] == ContainerStatusItem(
+        container_key='MEDU4550625',
+        description='Empty in Container Yard',
+        local_date_time='24/10/2019',
+        location=LocationItem(name='LONG BEACH, CA, US'),
+        vessel=None,
+        voyage=None,
+        est_or_actual='A',
+        task_id='1',
+    )
+
+    assert results[22] == ContainerItem(
+        container_key='TGCU0233249',
+        container_no='TGCU0233249',
+        task_id='1',
+    )
+
+    assert results[33] == MblItem(
+        mbl_no='MEDUMY898253',
+        pol=LocationItem(name='TANJUNG PELEPAS, MY'),
+        pod=LocationItem(name='LOS ANGELES, US'),
+        etd='17/09/2019',
+        vessel='GUDRUN MAERSK',
+        place_of_deliv=LocationItem(name='LOS ANGELES, US'),
+        latest_update='04.03.2020 at 04:05 W. Europe Standard Time',
+        task_id='1',
+    )
---
+++ test/spiders/carrier_mscu_multi/main_info/test_main_info.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+import pytest
+from scrapy import Request
+from scrapy.http import TextResponse
+
+from crawler.core_carrier.base import SHIPMENT_TYPE_MBL
+from crawler.core_carrier.exceptions import CarrierInvalidSearchNoError
+from crawler.spiders.carrier_mscu_multi import MainRoutingRule
+from test.spiders.carrier_mscu_multi import main_info
+
+
+@pytest.fixture
+def sample_loader(sample_loader):
+    sample_path = Path(__file__).parent
+    sample_loader.setup(sample_package=main_info, sample_path=sample_path)
+    return sample_loader
+
+
+@pytest.mark.parametrize(
+    'sub,mbl_no,',
+    [
+        ('01_without_ts_port', 'MEDUN4194175'),
+        ('02_not_arrival_yet', '177NDGNENX03449A'),
+        ('03_multi_containers', 'MEDUMY898253'),
+    ],
+)
+def test_main_info_routing_rule(sub, mbl_no, sample_loader):
+    http_text = sample_loader.read_file(sub, 'sample.html')
+
+    url = 'https://www.msc.com/track-a-shipment?agencyPath=twn'
+    response = TextResponse(
+        url=url,
+        body=http_text,
+        encoding='utf-8',
+        request=Request(
+            url=url,
+            meta={
+                'search_no': mbl_no,
+                'task_id': '1',
+            }
+        ),
+    )
+
+    rule = MainRoutingRule(search_type=SHIPMENT_TYPE_MBL)
+
+    results = list(rule.handle(response=response))
+
+    verify_module = sample_loader.load_sample_module(sub, 'verify')
+    verify_module.verify(results=results)
+
+
+@pytest.mark.parametrize('sub,mbl_no,expect_exception', [
+    ('e01_invalid_mbl_no', 'MEDUMY898252', CarrierInvalidSearchNoError),
+])
+def test_main_info_handler_mbl_no_error(sub, mbl_no, expect_exception, sample_loader):
+    http_text = sample_loader.read_file(sub, 'sample.html')
+
+    url = 'https://www.msc.com/track-a-shipment?agencyPath=twn'
+    response = TextResponse(
+        url=url,
+        body=http_text,
+        encoding='utf-8',
+        request=Request(
+            url=url,
+        ),
+    )
+
+    rule = MainRoutingRule(search_type=SHIPMENT_TYPE_MBL)
+
+    with pytest.raises(expect_exception):
+        list(rule.handle(response=response))
