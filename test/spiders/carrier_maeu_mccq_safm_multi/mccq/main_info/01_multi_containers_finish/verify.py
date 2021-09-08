9994437bc21e0f99e94b12364986ee9e7c1d504a
FloraTesng <flora.tseng@gofreight.co>
2021-09-08 15:40:23 +0800

    test(carrier_maeu_mccq_safm_multi): add test

---
+++ test/spiders/carrier_maeu_mccq_safm_multi/mccq/main_info/01_multi_containers_finish/verify.py
@@ -0,0 +1,68 @@
+from crawler.core_carrier.items import MblItem, ContainerItem, ContainerStatusItem, LocationItem
+
+
+def verify(results):
+    assert results[0] == MblItem(
+        mbl_no='589898475',
+        por=LocationItem(name='Beilun container terminal, Phase 2 -- Ningbo (Zhejiang, CN)'),
+        final_dest=LocationItem(name='VIP Greenport Joint Stock Company -- Haiphong (Hai Phong, VN)'),
+        task_id='1',
+    )
+
+    assert results[1] == ContainerItem(
+        container_key='PONU1767353',
+        container_no='PONU1767353',
+        final_dest_eta='2019-10-31T04:34:00.000',
+        task_id='1',
+    )
+
+    assert results[2] == ContainerStatusItem(
+        container_key='PONU1767353',
+        description='GATE-OUT-EMPTY',
+        local_date_time='2019-10-06T14:09:00.000',
+        location=LocationItem(name='NIngbo Beilun Donghwa Cont. Transp. -- Ningbo (Zhejiang, CN)'),
+        vessel='MAERSK ATLANTIC 3MY',
+        voyage='941E',
+        est_or_actual='A',
+        task_id='1',
+    )
+
+    assert results[8] == ContainerStatusItem(
+        container_key='PONU1767353',
+        description='GATE-OUT',
+        local_date_time='2019-11-08T00:56:00.000',
+        location=LocationItem(name='VIP Greenport Joint Stock Company -- Haiphong (Hai Phong, VN)'),
+        vessel='OREA M38',
+        voyage='943N',
+        est_or_actual='A',
+        task_id='1',
+    )
+
+    assert results[9] == ContainerItem(
+        container_key='PONU1947308',
+        container_no='PONU1947308',
+        final_dest_eta='2019-10-31T04:37:00.000',
+        task_id='1',
+    )
+
+    assert results[10] == ContainerStatusItem(
+        container_key='PONU1947308',
+        description='GATE-OUT-EMPTY',
+        local_date_time='2019-10-05T20:39:00.000',
+        location=LocationItem(name='NIngbo Beilun Donghwa Cont. Transp. -- Ningbo (Zhejiang, CN)'),
+        vessel='MAERSK ATLANTIC 3MY',
+        voyage='941E',
+        est_or_actual='A',
+        task_id='1',
+    )
+
+    assert results[16] == ContainerStatusItem(
+        container_key='PONU1947308',
+        description='GATE-OUT',
+        local_date_time='2019-11-08T04:19:00.000',
+        location=LocationItem(name='VIP Greenport Joint Stock Company -- Haiphong (Hai Phong, VN)'),
+        vessel='OREA M38',
+        voyage='943N',
+        est_or_actual='A',
+        task_id='1',
+    )
---
+++ test/spiders/carrier_maeu_mccq_safm_multi/safm/main_info/02_multi_containers_not_finish/verify.py
@@ -0,0 +1,68 @@
+from crawler.core_carrier.items import MblItem, ContainerItem, ContainerStatusItem, LocationItem
+
+
+def verify(results):
+    assert results[0] == MblItem(
+        mbl_no='712044685',
+        por=LocationItem(name='Kaohsiung Hanjin Terminal Pier76-78 -- Kaohsiung (TW)'),
+        final_dest=LocationItem(name='AICT -- Alexandria (EG)'),
+        task_id='1',
+    )
+
+    assert results[1] == ContainerItem(
+        container_key='FCIU4198511',
+        container_no='FCIU4198511',
+        final_dest_eta='2019-12-18T14:01:00.000',
+        task_id='1',
+    )
+
+    assert results[2] == ContainerStatusItem(
+        container_key='FCIU4198511',
+        description='GATE-OUT-EMPTY',
+        local_date_time='2019-11-08T15:26:00.000',
+        location=LocationItem(name='Kaohsiung Hanjin Terminal Pier76-78 -- Kaohsiung (TW)'),
+        vessel='MAERSK ARAS F1Y',
+        voyage='945W',
+        est_or_actual='A',
+        task_id='1',
+    )
+
+    assert results[10] == ContainerStatusItem(
+        container_key='FCIU4198511',
+        description='GATE-OUT',
+        local_date_time='2019-12-18T14:01:00.000',
+        location=LocationItem(name='AICT -- Alexandria (EG)'),
+        vessel='WADI ALRAYAN B22',
+        voyage='949W',
+        est_or_actual='E',
+        task_id='1',
+    )
+
+    assert results[11] == ContainerItem(
+        container_key='MRSU0260637',
+        container_no='MRSU0260637',
+        final_dest_eta='2019-12-18T14:01:00.000',
+        task_id='1',
+    )
+
+    assert results[12] == ContainerStatusItem(
+        container_key='MRSU0260637',
+        description='GATE-OUT-EMPTY',
+        local_date_time='2019-11-08T16:54:00.000',
+        location=LocationItem(name='Kaohsiung Hanjin Terminal Pier76-78 -- Kaohsiung (TW)'),
+        vessel='MAERSK ARAS F1Y',
+        voyage='945W',
+        est_or_actual='A',
+        task_id='1',
+    )
+
+    assert results[20] == ContainerStatusItem(
+        container_key='MRSU0260637',
+        description='GATE-OUT',
+        local_date_time='2019-12-18T14:01:00.000',
+        location=LocationItem(name='AICT -- Alexandria (EG)'),
+        vessel='WADI ALRAYAN B22',
+        voyage='949W',
+        est_or_actual='E',
+        task_id='1',
+    )
