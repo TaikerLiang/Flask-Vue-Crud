a75d5b2a19964823feb9fe3a297500e161e6e6e7
Paul Liang <paul.liang@hardcoretech.co>
2021-09-24 14:18:40 +0800

    fix(internal): update the proxy setting in rail and fix unit-test error

--- src/crawler/core_rail/request_helpers.py
+++ src/crawler/core_rail/request_helpers.py
@@ -13,9 +13,9 @@ from .exceptions import ProxyMaxRetryError
 
 @dataclasses.dataclass
 class RequestOption:
-    METHOD_GET = 'GET'
-    METHOD_POST_FORM = 'POST_FORM'
-    METHOD_POST_BODY = 'METHOD_POST_BODY'
+    METHOD_GET = "GET"
+    METHOD_POST_FORM = "POST_FORM"
+    METHOD_POST_BODY = "METHOD_POST_BODY"
 
     rule_name: str
     method: str
@@ -23,7 +23,7 @@ class RequestOption:
     headers: Dict = dataclasses.field(default_factory=dict)
     cookies: Dict = dataclasses.field(default_factory=dict)
     form_data: Dict = dataclasses.field(default_factory=dict)
-    body: str = ''
+    body: str = ""
     meta: Dict = dataclasses.field(default_factory=dict)
 
     def copy_and_extend_by(self, headers=None, meta=None) -> RequestOption:
@@ -31,29 +31,19 @@ class RequestOption:
             rule_name=self.rule_name,
             method=self.method,
             url=self.url,
-            headers={
-                **self.headers,
-                **(headers or {}),
-            },
-            cookies={
-                **self.cookies,
-            },
-            form_data={
-                **self.form_data,
-            },
+            headers={**self.headers, **(headers or {}),},
+            cookies={**self.cookies,},
+            form_data={**self.form_data,},
             body=self.body,
-            meta={
-                **self.meta,
-                **(meta or {}),
-            },
+            meta={**self.meta, **(meta or {}),},
         )
 
 
 # -------------------------------------------------------------------------------
 
 
-PROXY_GROUP_SHADER = 'SHADER'
-PROXY_GROUP_RESIDENTIAL = 'RESIDENTIAL'
+PROXY_GROUP_SHADER = "SHADER"
+PROXY_GROUP_RESIDENTIAL = "RESIDENTIAL"
 
 
 @dataclasses.dataclass
@@ -63,53 +53,46 @@ class ProxyOption:
 
 
 class ProxyManager:
-    PROXY_URL = 'http://proxy.apify.com:8000'
-    PROXY_PASSWORD = 'XZTBLpciyyTCFb3378xWJbuYY'
+    PROXY_URL = "http://proxy.apify.com:8000"
+    PROXY_PASSWORD = "XZTBLpciyyTCFb3378xWJbuYY"
 
     def __init__(self, session: str, logger: Logger):
         self._logger = logger
 
         self._proxy_options = [
-            ProxyOption(group=PROXY_GROUP_SHADER, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_SHADER, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_SHADER, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'{session}{self._generate_random_string()}'),
-            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f'{session}{self._generate_random_string()}'),
+            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f"{session}{self._generate_random_string()}"),
+            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f"{session}{self._generate_random_string()}"),
+            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f"{session}{self._generate_random_string()}"),
+            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f"{session}{self._generate_random_string()}"),
+            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f"{session}{self._generate_random_string()}"),
+            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f"{session}{self._generate_random_string()}"),
+            ProxyOption(group=PROXY_GROUP_RESIDENTIAL, session=f"{session}{self._generate_random_string()}"),
         ]
 
-        self._proxy_username = ''
+        self._proxy_username = ""
 
     @staticmethod
     def _generate_random_string():
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+        return "".join(random.choices(string.ascii_uppercase + string.digits, k=20))
 
     def renew_proxy(self):
         if not self._proxy_options:
             raise ProxyMaxRetryError()
 
         option = self._proxy_options.pop(0)
-        self._logger.warning(f'----- renew proxy ({len(self._proxy_options)}) {option}')
+        self._logger.warning(f"----- renew proxy ({len(self._proxy_options)}) {option}")
 
-        self._proxy_username = f'groups-{option.group},session-{option.session}'
+        self._proxy_username = f"groups-{option.group},session-{option.session}"
 
     def apply_proxy_to_request_option(self, option: RequestOption) -> RequestOption:
         return option.copy_and_extend_by(
-            headers={
-                'Proxy-Authorization': basic_auth_header(self._proxy_username, self.PROXY_PASSWORD),
-            },
-            meta={
-                'proxy': self.PROXY_URL,
-            },
+            headers={"Proxy-Authorization": basic_auth_header(self._proxy_username, self.PROXY_PASSWORD),},
+            meta={"proxy": self.PROXY_URL,},
         )
 
     def get_phantom_js_service_args(self):
         return [
-            f'--proxy={self.PROXY_URL}',
-            '--proxy-type=http',
-            f'--proxy-auth={self._proxy_username}:{self.PROXY_PASSWORD}',
+            f"--proxy={self.PROXY_URL}",
+            "--proxy-type=http",
+            f"--proxy-auth={self._proxy_username}:{self.PROXY_PASSWORD}",
         ]
