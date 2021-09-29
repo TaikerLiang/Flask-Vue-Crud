e189ae8f0eebfb3056bbc236ced79932edc5e3eb
Leo Tsao <leo.tsao@gofreight.co>
2021-09-29 10:07:01 +0800

    test(air_china_airline): add test for HistoryItem

--- test/spiders/air_china_airline/search/01_basic/verify.py
+++ test/spiders/air_china_airline/search/01_basic/verify.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from crawler.core_air.items import AirItem, FlightItem
+from crawler.core_air.items import AirItem, FlightItem, HistoryItem
 
 
 def verify(results: List):
@@ -23,4 +23,24 @@ def verify(results: List):
         weight="1088 KG",
         atd="01Aug 02:22",
         ata="01Aug 04:22",
+    )
+
+    assert results[6] == HistoryItem(
+        task_id="1",
+        status="Received",
+        flight_no="CI 5118/03Aug",
+        pieces="5",
+        weight="",
+        location="LAX",
+        time="03Aug 08:11",
+    )
+
+    assert results[11] == HistoryItem(
+        task_id="1",
+        status="Departed",
+        flight_no="CI 5896/01Aug",
+        pieces="5",
+        weight="1088kg",
+        location="CAN",
+        time="01Aug 02:22",
     )
