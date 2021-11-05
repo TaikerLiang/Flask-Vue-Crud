449c9ed60196950f33cfde32ab6064cddd2765bd
Leo Tsao <leo.tsao@gofreight.co>
2021-11-05 11:06:36 +0800

    refactor(carrier_mscu): refactor table locator
    [why] code reuse
    [how]

--- src/crawler/spiders/carrier_mscu.py
+++ src/crawler/spiders/carrier_mscu.py
@@ -26,8 +26,8 @@ from crawler.core_carrier.items import (
 )
 from crawler.core_carrier.request_helpers import RequestOption
 from crawler.core_carrier.rules import BaseRoutingRule, RuleManager
+from crawler.core.table import BaseTable, TableExtractor
 from crawler.extractors.table_cell_extractors import FirstTextTdExtractor
-from crawler.extractors.table_extractors import BaseTableLocator, HeaderMismatchError, TableExtractor
 
 URL = "https://www.msc.com"
 
@@ -288,10 +288,10 @@ class Extractor:
         td_extractor = FirstTextTdExtractor()
 
         return {
-            "pol": table_extractor.extract_cell(top="Port of load", left=None, extractor=td_extractor),
-            "pod": table_extractor.extract_cell(top="Port of discharge", left=None, extractor=td_extractor),
-            "etd": table_extractor.extract_cell(top="Departure date", left=None, extractor=td_extractor),
-            "vessel": table_extractor.extract_cell(top="Vessel", left=None, extractor=td_extractor),
+            "pol": table_extractor.extract_cell(top="Port of load", extractor=td_extractor),
+            "pod": table_extractor.extract_cell(top="Port of discharge", extractor=td_extractor),
+            "etd": table_extractor.extract_cell(top="Departure date", extractor=td_extractor),
+            "vessel": table_extractor.extract_cell(top="Vessel", extractor=td_extractor),
         }
 
     @staticmethod
@@ -348,9 +348,8 @@ class Extractor:
         table_locator = ContainerInfoTableLocator()
         table_locator.parse(table=table_selector)
         table_extractor = TableExtractor(table_locator=table_locator)
-        td_extractor = FirstTextTdExtractor()
 
-        return table_extractor.extract_cell(top="Shipped to", left=None, extractor=td_extractor)
+        return table_extractor.extract_cell(top="Shipped to")
 
     @staticmethod
     def extract_container_status_list(container_selector_map: Dict[str, scrapy.Selector]):
@@ -401,7 +400,7 @@ class Extractor:
         return m.group("latest_update").strip()
 
 
-class MainInfoTableLocator(BaseTableLocator):
+class MainInfoTableLocator(BaseTable):
     """
     +-----------+-----------+-----+-----------+ <thead>      -+
     | Title A-1 | Title A-2 | ... | Title A-N | <tr> <th>     |
@@ -414,203 +413,73 @@ class MainInfoTableLocator(BaseTableLocator):
     +-----------+-----------+-----+-----------+              -+
     """
 
-    def __init__(self):
-        self._td_map = {}  # top_header: td
-
     def parse(self, table: scrapy.Selector):
-        thead_list = table.css("thead")
-        tbody_list = table.css("tbody")
+        title_th_list = table.css("thead tr th")
+        title_text_list = [self._extract_top(th=th) for th in title_th_list]
+        data_td_list = table.css("tbody tr td")
 
-        for thead_index, thead in enumerate(thead_list):
-            tbody_index = thead_index
-            tbody = tbody_list[tbody_index]
-
-            th_list = thead.css("tr th")
-            td_list = tbody.css("tr td")
-
-            for th_index, th in enumerate(th_list):
-                td_index = th_index
-                td = td_list[td_index]
-
-                top = self._extract_top(th=th)
-                self._td_map[top] = td
+        for index, (title, data_td) in enumerate(zip(title_text_list, data_td_list)):
+            self._left_header_set.add(index)
+            self._td_map.setdefault(title, [])
+            self._td_map[title].append(data_td)
 
     @staticmethod
     def _extract_top(th):
         th_text = th.css("::text").get()
         return th_text.strip() if isinstance(th_text, str) else ""
 
-    def get_cell(self, top, left) -> scrapy.Selector:
-        assert left is None
-        try:
-            return self._td_map[top]
-        except (KeyError, IndexError) as err:
-            raise HeaderMismatchError(repr(err))
 
-    def has_header(self, top=None, left=None) -> bool:
-        return (top in self._td_map) and (left is None)
-
-
-class ContainerInfoTableLocator(BaseTableLocator):
+class ContainerInfoTableLocator(BaseTable):
     """
-    +-----------+-----------+-----+-----------+ <tbody>      -+
+    +-----------+-----------+-----+-----------+ <thead>      -+
     | Title A-1 | Title A-2 | ... | Title A-N | <tr> <th>     |
-    +-----------+-----------+-----+-----------+               | A
+    +-----------+-----------+-----+-----------+ <tbody>       | A
     | Cell A-1  | Cell A-2  | ... | Cell A-N  | <tr> <td>     |
-    +-----------+-----------+-----+-----------+              -+
+    +-----------+-----------+-----+-----------+ <thead>      -+
     | Title B-1 | Title B-2 | ... | Title B-N | <tr> <th>     |
-    +-----------+-----------+-----+-----------+               | B
+    +-----------+-----------+-----+-----------+ <tbody>       | B
     | Cell B-1  | Cell B-2  | ... | Cell B-N  | <tr> <td>     |
-    +-----------+-----------+-----+-----------+ <\tbody>     -+
+    +-----------+-----------+-----+-----------+              -+
     """
 
-    def __init__(self):
-        self._td_map = {}  # top_header: td
-
     def parse(self, table: scrapy.Selector):
-        th_row_list = table.xpath(".//th/parent::tr")
-        td_row_list = table.xpath(".//td/parent::tr")
+        # title_th_list = table.xpath(".//th/parent::tr/th")
+        title_th_list = table.css("thead tr th")
+        title_text_list = [self._extract_top(th=th) for th in title_th_list]
+        data_td_list = table.css("tbody tr td")
 
-        for th_row_index, th_row in enumerate(th_row_list):
-            td_row_index = th_row_index
-            td_row = td_row_list[td_row_index]
-
-            th_list = th_row.css("th")
-            td_list = td_row.css("td")
-
-            for th_index, th in enumerate(th_list):
-                td_index = th_index
-                td = td_list[td_index]
-
-                top = self._extract_top(th=th)
-                self._td_map[top] = td
+        for index, (title, data_td) in enumerate(zip(title_text_list, data_td_list)):
+            self._left_header_set.add(index)
+            self._td_map.setdefault(title, [])
+            self._td_map[title].append(data_td)
 
     @staticmethod
     def _extract_top(th):
         th_text = th.css("::text").get()
         return th_text.strip() if isinstance(th_text, str) else ""
 
-    def get_cell(self, top, left) -> scrapy.Selector:
-        assert left is None
-        try:
-            return self._td_map[top]
-        except (KeyError, IndexError) as err:
-            raise HeaderMismatchError(repr(err))
 
-    def has_header(self, top=None, left=None) -> bool:
-        return (top in self._td_map) and (left is None)
-
-
-class ContainerStatusTableLocator(BaseTableLocator):
-
+class ContainerStatusTableLocator(BaseTable):
     STATUS_TOP = "STATUS"
 
-    def __init__(self):
-        self._td_map = {}  # top_header: [td, ...]
-        self._data_len = 0
-
     def parse(self, table: scrapy.Selector):
-        th_list = table.css("thead th")
+        title_th_list = table.css("thead th")
+        title_text_list = [self._extract_top(th=th) for th in title_th_list]
         data_tr_list = table.css("tbody tr")
 
-        for th_index, th in enumerate(th_list):
-            top_header = self._extract_top_header(th=th)
-            self._td_map[top_header] = []
+        for index, data_tr in enumerate(data_tr_list):
+            status = data_tr.css("::attr(class)").get()
+            status_td = scrapy.Selector(text=f"<td>{status}</td>")
+            self._td_map.setdefault(self.STATUS_TOP, [])
+            self._td_map[self.STATUS_TOP].append(status_td)
 
-            data_index = th_index
-            for data_tr in data_tr_list:
-                data_td = data_tr.css("td")[data_index]
-                self._td_map[top_header].append(data_td)
-
-        tr_class_name_list = [data_tr.css("::attr(class)").get() for data_tr in data_tr_list]
-        status_td_list = [scrapy.Selector(text=f"<td>{tr_class_name}</td>") for tr_class_name in tr_class_name_list]
-        self._td_map[self.STATUS_TOP] = status_td_list
-
-        self._data_len = len(data_tr_list)
+            data_td_list = data_tr.css("td")
+            self._left_header_set.add(index)
+            for title, data_td in zip(title_text_list, data_td_list):
+                self._td_map.setdefault(title, [])
+                self._td_map[title].append(data_td)
 
     @staticmethod
-    def _extract_top_header(th):
+    def _extract_top(th):
         top_header = th.css("::text").get()
         return top_header.strip() if isinstance(top_header, str) else ""
-
-    def get_cell(self, top, left) -> scrapy.Selector:
-        try:
-            return self._td_map[top][left]
-        except (KeyError, IndexError) as err:
-            raise HeaderMismatchError(repr(err))
-
-    def has_header(self, top=None, left=None) -> bool:
-        return (top in self._td_map) and (left is None)
-
-    def iter_left_header(self):
-        for i in range(self._data_len):
-            yield i
-
-
-class MscuCarrierChromeDriver:
-    def __init__(self):
-        prefs = {
-            "profile.default_content_setting_values": {"images": 2},
-        }
-
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("prefs", prefs)
-        options.add_argument("--disable-plugins")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--headless")
-        options.add_argument("window-size=1024x768")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-infobars")
-
-        self._chrome_driver = webdriver.Chrome(chrome_options=options)
-
-    def search_mbl_no(self, mbl_no):
-        track_url = f"{URL}/track-a-shipment?agencyPath=twn"
-        self._chrome_driver.get(url=track_url)
-
-        search_bar_css_query = "input#ctl00_ctl00_plcMain_plcMain_TrackSearch_txtBolSearch_TextField"
-        search_bar_locator = (By.CSS_SELECTOR, search_bar_css_query)
-
-        search_button_css_query = "a#ctl00_ctl00_plcMain_plcMain_TrackSearch_hlkSearch"
-        search_button_locator = (By.CSS_SELECTOR, search_button_css_query)
-
-        try:
-            WebDriverWait(self._chrome_driver, 10).until(EC.presence_of_element_located(search_bar_locator))
-            WebDriverWait(self._chrome_driver, 10).until(EC.element_to_be_clickable(search_button_locator))
-        except TimeoutException:
-            raise LoadWebsiteTimeOutError(url=self._chrome_driver.current_url)
-
-        search_bar = self._chrome_driver.find_element_by_css_selector(search_bar_css_query)
-        search_button = self._chrome_driver.find_element_by_css_selector(search_button_css_query)
-        search_bar.send_keys(mbl_no)
-
-        pop_up_reject_btn_locator = (By.CSS_SELECTOR, "a#ctl00_ctl00_ucNewsetterSignupPopup_btnReject")
-        if self._check_element_clickable(pop_up_reject_btn_locator):
-            pop_up_reject_btn = self._chrome_driver.find_element(pop_up_reject_btn_locator)
-            pop_up_reject_btn.click()
-
-        search_button.click()
-
-    def get_body_text(self):
-        mbl_no_locator = (By.CSS_SELECTOR, "div#ctl00_ctl00_plcMain_plcMain_pnlTrackingResults")
-
-        try:
-            WebDriverWait(self._chrome_driver, 10).until(EC.presence_of_element_located(mbl_no_locator))
-        except TimeoutException:
-            raise LoadWebsiteTimeOutError(url=self._chrome_driver.current_url)
-
-        body = self._chrome_driver.find_element_by_css_selector("body")
-        body_text = body.get_attribute("outerHTML")
-
-        return body_text
-
-    def close(self):
-        self._chrome_driver.close()
-
-    def _check_element_clickable(self, locator):
-        try:
-            return bool(EC.element_to_be_clickable(locator)(self._chrome_driver))
-        except NoSuchElementException:
-            return False
