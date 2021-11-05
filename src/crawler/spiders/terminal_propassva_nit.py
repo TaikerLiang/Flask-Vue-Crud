67e4ba4e1af7af7c06222400b484622576b99a29
noreason <ken5112840801@gmail.com>
2021-11-05 17:52:20 +0800

    feat(propassva_nit_spider): Add propassva_vig_spider

---
+++ src/crawler/spiders/terminal_propassva_nit.py
@@ -0,0 +1,6 @@
+from crawler.core_terminal.propassva_share_spider import PropassvaShareSpider
+
+
+class TerminalPropassvaNitSpider(PropassvaShareSpider):
+    firms_code = "L005"
+    name = "terminal_propassva_nit"
