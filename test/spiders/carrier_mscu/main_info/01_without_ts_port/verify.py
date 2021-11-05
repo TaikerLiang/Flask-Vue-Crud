8f43cf4fbc6b6bfa4c2e3630afd5f34bc5babf12
Leo Tsao <leo.tsao@gofreight.co>
2021-11-05 11:07:36 +0800

    test(carrier_mscu): update test
    [why] html files out of date
    [how] replaced with new ones

--- test/spiders/carrier_mscu/main_info/01_without_ts_port/verify.py
+++ test/spiders/carrier_mscu/main_info/01_without_ts_port/verify.py
@@ -3,36 +3,36 @@ from crawler.core_carrier.items import MblItem, ContainerItem, ContainerStatusIt
 
 def verify(results):
     assert results[0] == ContainerItem(
-        container_key='GLDU7632978',
-        container_no='GLDU7632978',
+        container_key="GLDU7632978",
+        container_no="GLDU7632978",
     )
 
     assert results[1] == ContainerStatusItem(
-        container_key='GLDU7632978',
-        description='Empty in Container Yard',
-        local_date_time='10/10/2019',
-        location=LocationItem(name='LOS ANGELES, CA, US'),
+        container_key="GLDU7632978",
+        description="Empty received at CY",
+        local_date_time="10/10/2019",
+        location=LocationItem(name="LOS ANGELES, US"),
         vessel=None,
         voyage=None,
-        est_or_actual='A',
+        est_or_actual="A",
     )
 
     assert results[10] == ContainerStatusItem(
-        container_key='GLDU7632978',
-        description='Empty to Shipper',
-        local_date_time='04/09/2019',
-        location=LocationItem(name='ZHAPU, 33, CN'),
+        container_key="GLDU7632978",
+        description="Export at barge yard",
+        local_date_time="05/09/2019",
+        location=LocationItem(name="ZHAPU, CN"),
         vessel=None,
         voyage=None,
-        est_or_actual='A',
+        est_or_actual="A",
     )
 
-    assert results[11] == MblItem(
-        mbl_no='MEDUN4194175',
-        pol=LocationItem(name='NINGBO, CN'),
-        pod=LocationItem(name='LONG BEACH, US'),
-        etd='18/09/2019',
-        vessel='MSC BERYL',
-        place_of_deliv=LocationItem(name='LONG BEACH, US'),
-        latest_update='04.03.2020 at 03:33 W. Europe Standard Time',
+    assert results[12] == MblItem(
+        mbl_no="MEDUN4194175",
+        pol=LocationItem(name="NINGBO, CN"),
+        pod=LocationItem(name="LONG BEACH, US"),
+        etd="18/09/2019",
+        vessel="MSC BERYL",
+        place_of_deliv=LocationItem(name="LONG BEACH, US"),
+        latest_update="05.11.2021 at 03:23 Central Europe Standard Time",
     )
--- test/spiders/carrier_mscu/main_info/03_multi_containers/verify.py
+++ test/spiders/carrier_mscu/main_info/03_multi_containers/verify.py
@@ -3,46 +3,46 @@ from crawler.core_carrier.items import MblItem, ContainerItem, ContainerStatusIt
 
 def verify(results):
     assert results[0] == ContainerItem(
-        container_key='GLDU7636572',
-        container_no='GLDU7636572',
+        container_key="MEDU4550625",
+        container_no="MEDU4550625",
     )
 
     assert results[1] == ContainerStatusItem(
-        container_key='GLDU7636572',
-        description='Empty in Container Yard',
-        local_date_time='23/10/2019',
-        location=LocationItem(name='LONG BEACH, CA, US'),
+        container_key="MEDU4550625",
+        description="Empty received at CY",
+        local_date_time="24/10/2019",
+        location=LocationItem(name="LONG BEACH, US"),
         vessel=None,
         voyage=None,
-        est_or_actual='A',
+        est_or_actual="A",
     )
 
-    assert results[11] == ContainerItem(
-        container_key='MEDU4550625',
-        container_no='MEDU4550625',
+    assert results[12] == ContainerItem(
+        container_key="GLDU7636572",
+        container_no="GLDU7636572",
     )
 
-    assert results[12] == ContainerStatusItem(
-        container_key='MEDU4550625',
-        description='Empty in Container Yard',
-        local_date_time='24/10/2019',
-        location=LocationItem(name='LONG BEACH, CA, US'),
+    assert results[13] == ContainerStatusItem(
+        container_key="GLDU7636572",
+        description="Empty received at CY",
+        local_date_time="23/10/2019",
+        location=LocationItem(name="LONG BEACH, US"),
         vessel=None,
         voyage=None,
-        est_or_actual='A',
+        est_or_actual="A",
     )
 
-    assert results[22] == ContainerItem(
-        container_key='TGCU0233249',
-        container_no='TGCU0233249',
+    assert results[24] == ContainerItem(
+        container_key="TGCU0233249",
+        container_no="TGCU0233249",
     )
 
-    assert results[33] == MblItem(
-        mbl_no='MEDUMY898253',
-        pol=LocationItem(name='TANJUNG PELEPAS, MY'),
-        pod=LocationItem(name='LOS ANGELES, US'),
-        etd='17/09/2019',
-        vessel='GUDRUN MAERSK',
-        place_of_deliv=LocationItem(name='LOS ANGELES, US'),
-        latest_update='04.03.2020 at 04:05 W. Europe Standard Time',
+    assert results[-1] == MblItem(
+        mbl_no="MEDUMY898253",
+        pol=LocationItem(name="TANJUNG PELEPAS, MY"),
+        pod=LocationItem(name="LOS ANGELES, US"),
+        etd="17/09/2019",
+        vessel="GUDRUN MAERSK",
+        place_of_deliv=LocationItem(name="LOS ANGELES, US"),
+        latest_update="05.11.2021 at 03:30 Central Europe Standard Time",
     )
