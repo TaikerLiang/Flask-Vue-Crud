402d6475aa2cb788d84b151f379d3e4c09964752
Leo Tsao <leo.tsao@gofreight.co>
2021-11-26 18:34:55 +0800

    test(carrier): fix tests

--- test/spiders/carrier_aplu/main_info/test_main_info.py
+++ test/spiders/carrier_aplu/main_info/test_main_info.py
@@ -20,69 +20,72 @@ def sample_loader(sample_loader):
 
 
 @pytest.mark.parametrize(
-    'sub,mbl_no',
+    "sub,mbl_no",
     [
-        ('01_not_finish', 'AXK0185154'),
-        ('02_finish', 'XHMN810789'),
-        ('03_multiple_containers', 'SHSE015942'),
-        ('04_por_dest', 'AWB0135426'),
-        ('05_pod_status_is_remaining', 'NANZ001007'),
-        ('06_container_status_has_extra_tr', 'AYU0320031'),
-        ('07_data_not_found', 'SLHE21050437'),
+        ("01_not_finish", "AXK0185154"),
+        ("02_finish", "XHMN810789"),
+        ("03_multiple_containers", "SHSE015942"),
+        ("04_por_dest", "AWB0135426"),
+        ("05_pod_status_is_remaining", "NANZ001007"),
+        ("06_container_status_has_extra_tr", "AYU0320031"),
+        ("07_data_not_found", "SLHE21050437"),
     ],
 )
 def test_first_tier_routing_rule(sample_loader, sub, mbl_no):
-    html_text = sample_loader.read_file(sub, 'main_info.html')
+    html_text = sample_loader.read_file(sub, "main_info.html")
 
     option = FirstTierRoutingRule.build_request_option(
-        search_no=mbl_no, base_url=CarrierApluSpider.base_url, search_type=SHIPMENT_TYPE_MBL)
+        search_no=mbl_no, base_url=CarrierApluSpider.base_url, search_type=SHIPMENT_TYPE_MBL
+    )
 
     response = TextResponse(
         url=option.url,
-        encoding='utf-8',
+        encoding="utf-8",
         body=html_text,
         request=Request(
             url=option.url,
             meta=option.meta,
-        )
+        ),
     )
 
     routing_rule = FirstTierRoutingRule(search_type=SHIPMENT_TYPE_MBL)
     results = list(routing_rule.handle(response=response))
 
-    verify_module = sample_loader.load_sample_module(sub, 'verify')
+    verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.verify(results=results)
 
+
 @pytest.mark.parametrize(
-    'sub,mbl_no',
+    "sub,mbl_no",
     [
-        ('01_not_finish', 'AXK0185154'),
-        ('02_finish', 'XHMN810789'),
-        ('03_multiple_containers', 'SHSE015942'),
-        ('04_por_dest', 'AWB0135426'),
-        ('05_pod_status_is_remaining', 'NANZ001007'),
-        ('06_container_status_has_extra_tr', 'AYU0320031'),
-        ('07_data_not_found', 'SLHE21050437'),
+        ("01_not_finish", "AXK0185154"),
+        ("02_finish", "XHMN810789"),
+        ("03_multiple_containers", "SHSE015942"),
+        ("04_por_dest", "AWB0135426"),
+        ("05_pod_status_is_remaining", "NANZ001007"),
+        ("06_container_status_has_extra_tr", "AYU0320031"),
+        ("07_data_not_found", "SLHE21050437"),
     ],
 )
 def test_multi_first_tier_routing_rule(sample_loader, sub, mbl_no):
-    html_text = sample_loader.read_file(sub, 'main_info.html')
+    html_text = sample_loader.read_file(sub, "main_info.html")
 
     option = MultiFirstTierRoutingRule.build_request_option(
-        search_no=mbl_no, base_url=MultiCarrierApluSpider.base_url, search_type=SHIPMENT_TYPE_MBL, task_id=1)
+        search_nos=[mbl_no], base_url=MultiCarrierApluSpider.base_url, search_type=SHIPMENT_TYPE_MBL, task_ids=[1]
+    )
 
     response = TextResponse(
         url=option.url,
-        encoding='utf-8',
+        encoding="utf-8",
         body=html_text,
         request=Request(
             url=option.url,
             meta=option.meta,
-        )
+        ),
     )
 
     routing_rule = MultiFirstTierRoutingRule(search_type=SHIPMENT_TYPE_MBL)
     results = list(routing_rule.handle(response=response))
 
-    verify_module = sample_loader.load_sample_module(sub, 'verify')
+    verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.multi_verify(results=results)
