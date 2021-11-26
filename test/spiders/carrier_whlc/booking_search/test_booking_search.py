83afec4c456936213f935b7d67d63e31c1f33d45
Leo Tsao <leo.tsao@gofreight.co>
2021-11-26 14:50:50 +0800

    test(carrier_whlc): modify tests
    [why] pyppeteer refactor has been made
    [how]

--- test/spiders/carrier_whlc/booking_search/test_booking_search.py
+++ test/spiders/carrier_whlc/booking_search/test_booking_search.py
@@ -15,72 +15,72 @@ def sample_loader(sample_loader):
 
 
 @pytest.mark.parametrize(
-    'sub,booking_no',
+    "sub,booking_no",
     [
-        ('01_detail', '034B524357'),
+        ("01_detail", "034B524357"),
     ],
 )
 def test_extract_basic_info(sub, booking_no, sample_loader):
-    html_text = sample_loader.read_file(sub, 'detail_page.html')
+    html_text = sample_loader.read_file(sub, "detail_page.html")
 
     response_selector = Selector(text=html_text)
 
-    routing_rule = BookingRoutingRule()
+    routing_rule = BookingRoutingRule(content_getter=None)
     results = routing_rule._extract_basic_info(response_selector)
 
-    verify_module = sample_loader.load_sample_module(sub, 'verify')
+    verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.verify_basic(results=results)
 
 
 @pytest.mark.parametrize(
-    'sub,booking_no',
+    "sub,booking_no",
     [
-        ('01_detail', '034B524357'),
+        ("01_detail", "034B524357"),
     ],
 )
 def test_extract_vessel_info(sub, booking_no, sample_loader):
-    html_text = sample_loader.read_file(sub, 'detail_page.html')
+    html_text = sample_loader.read_file(sub, "detail_page.html")
 
     response_selector = Selector(text=html_text)
 
-    routing_rule = BookingRoutingRule()
+    routing_rule = BookingRoutingRule(content_getter=None)
     results = routing_rule._extract_vessel_info(response_selector)
 
-    verify_module = sample_loader.load_sample_module(sub, 'verify')
+    verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.verify_vessel(results=results)
 
 
 @pytest.mark.parametrize(
-    'sub,booking_no',
+    "sub,booking_no",
     [
-        ('01_detail', '034B524357'),
+        ("01_detail", "034B524357"),
     ],
 )
 def test_extract_container_no_and_status_links(sub, booking_no, sample_loader):
-    html_text = sample_loader.read_file(sub, 'detail_page.html')
+    html_text = sample_loader.read_file(sub, "detail_page.html")
 
     response_selector = Selector(text=html_text)
 
-    routing_rule = BookingRoutingRule()
+    routing_rule = BookingRoutingRule(content_getter=None)
     results = list(routing_rule._extract_container_no_and_status_links(response_selector))
 
-    verify_module = sample_loader.load_sample_module(sub, 'verify')
+    verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.verify_container_no(results=results)
 
 
 @pytest.mark.parametrize(
-    'sub,booking_no,container_no',
+    "sub,booking_no,container_no",
     [
-        ('02_history', '034B524357', 'TCKU7313477'),
+        ("02_history", "034B524357", "TCKU7313477"),
     ],
 )
 def test_extract_container_status(sub, booking_no, container_no, sample_loader):
-    html_text = sample_loader.read_file(sub, 'history_page.html')
+    html_text = sample_loader.read_file(sub, "history_page.html")
 
     response_selector = Selector(text=html_text)
 
-    routing_rule = BookingRoutingRule()
+    routing_rule = BookingRoutingRule(content_getter=None)
     results = list(routing_rule._extract_container_status(response_selector))
 
-    verify_module = sample_loader.load_sample_module(sub, 'verify')
+    verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.verify(results=results)
