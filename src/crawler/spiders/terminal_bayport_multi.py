044eda73074000daa1561ebb2a1836718a874ca1
Leo Tsao <leo.tsao@gofreight.co>
2021-12-03 11:31:08 +0800

    feat(terminal_bayport): add paging mechanism
    [why] extend running time
    [how] add a new rule for paging

--- src/crawler/spiders/terminal_bayport_multi.py
+++ src/crawler/spiders/terminal_bayport_multi.py
@@ -1,21 +1,21 @@
-from urllib.parse import urlencode, unquote
+from urllib.parse import urlencode
 from datetime import datetime
 import json
 import time
-from typing import List, Dict
+from typing import List
 
-from scrapy import Request, FormRequest, Selector
+from scrapy import Request, FormRequest
 
 from crawler.core_terminal.base_spiders import BaseMultiTerminalSpider
-from crawler.core_terminal.items import BaseTerminalItem, DebugItem, TerminalItem, InvalidContainerNoItem
+from crawler.core_terminal.items import DebugItem, TerminalItem, InvalidContainerNoItem
 from crawler.core_terminal.rules import RuleManager, BaseRoutingRule, RequestOption
 
-BASE_URL = 'https://csp.poha.com'
+BASE_URL = "https://csp.poha.com"
 
 
 class TerminalBayportMultiSpider(BaseMultiTerminalSpider):
-    firms_code = 'V136'
-    name = 'terminal_bayport_multi'
+    firms_code = "V136"
+    name = "terminal_bayport_multi"
 
     def __init__(self, *args, **kwargs):
         super(TerminalBayportMultiSpider, self).__init__(*args, **kwargs)
@@ -23,6 +23,7 @@ class TerminalBayportMultiSpider(BaseMultiTerminalSpider):
         rules = [
             LoginRoutingRule(),
             ContainerRoutingRule(),
+            NextRoundRoutingRule(),
         ]
 
         self._rule_manager = RuleManager(rules=rules)
@@ -33,7 +34,7 @@ class TerminalBayportMultiSpider(BaseMultiTerminalSpider):
         yield self._build_request_by(option=option)
 
     def parse(self, response):
-        yield DebugItem(info={'meta': dict(response.meta)})
+        yield DebugItem(info={"meta": dict(response.meta)})
 
         routing_rule = self._rule_manager.get_rule_by_response(response=response)
 
@@ -42,11 +43,11 @@ class TerminalBayportMultiSpider(BaseMultiTerminalSpider):
 
         for result in routing_rule.handle(response=response):
             if isinstance(result, TerminalItem) or isinstance(result, InvalidContainerNoItem):
-                c_no = result['container_no']
+                c_no = result["container_no"]
                 if c_no:
                     t_ids = self.cno_tid_map[c_no]
                     for t_id in t_ids:
-                        result['task_id'] = t_id
+                        result["task_id"] = t_id
                         yield result
             elif isinstance(result, RequestOption):
                 yield self._build_request_by(option=result)
@@ -83,39 +84,39 @@ class TerminalBayportMultiSpider(BaseMultiTerminalSpider):
                 dont_filter=True,
             )
         else:
-            raise ValueError(f'Invalid option.method [{option.method}]')
+            raise ValueError(f"Invalid option.method [{option.method}]")
 
 
 # -------------------------------------------------------------------------------
 
 
 class LoginRoutingRule(BaseRoutingRule):
-    name = 'Login'
+    name = "Login"
 
     @classmethod
     def build_request_option(cls, container_no_list: List[str]) -> RequestOption:
-        url = f'{BASE_URL}/Lynx/VITTerminalAccess/Login.aspx'
+        url = f"{BASE_URL}/Lynx/VITTerminalAccess/Login.aspx"
         form_data = {
-            'User': 'hard202006010',
-            'Pass': '*r@y39=9q-!k',
+            "User": "hard202006010",
+            "Pass": "*r@y39=9q-!k",
         }
 
         return RequestOption(
             rule_name=cls.name,
             method=RequestOption.METHOD_POST_BODY,
             headers={
-                'Content-Type': 'application/x-www-form-urlencoded',
+                "Content-Type": "application/x-www-form-urlencoded",
             },
             url=url,
             body=urlencode(form_data),
-            meta={'container_no_list': container_no_list},
+            meta={"container_no_list": container_no_list},
         )
 
     def get_save_name(self, response) -> str:
-        return f'{self.name}.html'
+        return f"{self.name}.html"
 
     def handle(self, response):
-        container_no_list = response.meta['container_no_list']
+        container_no_list = response.meta["container_no_list"]
         yield ContainerRoutingRule.build_request_option(container_no_list=container_no_list)
 
 
@@ -123,33 +124,31 @@ class LoginRoutingRule(BaseRoutingRule):
 
 
 class ContainerRoutingRule(BaseRoutingRule):
-    name = 'CONTAINER'
+    name = "CONTAINER"
 
     @classmethod
     def build_request_option(cls, container_no_list: List) -> RequestOption:
         params = {
-            'WhichReq': 'Container',
-            'ContainerNum': ','.join(container_no_list),
-            'BOLNum': '',
-            'PTD': '',
-            'ContainerNotification': False,
-            '_': cls.thirteen_digits_timestamp(),
+            "WhichReq": "Container",
+            "ContainerNum": ",".join(container_no_list[:20]),
+            "BOLNum": "",
+            "PTD": "",
+            "ContainerNotification": False,
+            "_": cls.thirteen_digits_timestamp(),
         }
         url = f"https://csp.poha.com/Lynx/VITTerminalAccess/GetReleaseInquiryList.aspx?{urlencode(params)}"
 
         return RequestOption(
-            rule_name=cls.name,
-            method=RequestOption.METHOD_GET,
-            url=url,
+            rule_name=cls.name, method=RequestOption.METHOD_GET, url=url, meta={"container_no_list": container_no_list}
         )
 
     def get_save_name(self, response) -> str:
-        return f'{self.name}.html'
+        return f"{self.name}.html"
 
     def handle(self, response):
         resp = json.loads(response.body)
 
-        for row in resp['aaData']:
+        for row in resp["aaData"]:
             yield TerminalItem(
                 container_no=row[2],
                 holds=row[5],
@@ -158,12 +157,15 @@ class ContainerRoutingRule(BaseRoutingRule):
                 last_free_day=self._get_last_free_day(row[9], row[10]),
             )
 
+        container_no_list = response.meta["container_no_list"]
+        yield NextRoundRoutingRule.build_request_option(container_no_list=container_no_list)
+
     def _get_last_free_day(self, port_lfd, line_lfd):
         port_lfd_dt, line_lfd_dt = None, None
         if port_lfd:
-            port_lfd_dt = datetime.strptime(port_lfd, '%m/%d/%Y')
+            port_lfd_dt = datetime.strptime(port_lfd, "%m/%d/%Y")
         if line_lfd:
-            line_lfd_dt = datetime.strptime(line_lfd, '%m/%d/%Y')
+            line_lfd_dt = datetime.strptime(line_lfd, "%m/%d/%Y")
 
         if port_lfd_dt and line_lfd_dt and port_lfd_dt < line_lfd_dt:
             return port_lfd
@@ -173,3 +175,28 @@ class ContainerRoutingRule(BaseRoutingRule):
     @staticmethod
     def thirteen_digits_timestamp():
         return round(time.time() * 1000)
+
+
+class NextRoundRoutingRule(BaseRoutingRule):
+    name = "NEXT_ROUND"
+
+    @classmethod
+    def build_request_option(cls, container_no_list: List) -> RequestOption:
+        return RequestOption(
+            rule_name=cls.name,
+            method=RequestOption.METHOD_GET,
+            url="https://www.google.com",
+            meta={
+                "container_no_list": container_no_list,
+            },
+        )
+
+    def handle(self, response):
+        container_no_list = response.meta["container_no_list"]
+
+        if len(container_no_list) <= 20:  # page size == 20
+            return
+
+        container_no_list = container_no_list[20:]
+
+        yield ContainerRoutingRule.build_request_option(container_no_list=container_no_list)
