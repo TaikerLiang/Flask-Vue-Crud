fa1437844b2e1aee0e01022a3af4c8b7d52a908e
Leo Tsao <leo.tsao@gofreight.co>
2022-01-18 15:20:01 +0800

    test(carrier_aplu): fix tests
    [why] new field in ContainerStatusItem
    [how]

--- test/spiders/carrier_aplu/container/01_basic/verify.py
+++ test/spiders/carrier_aplu/container/01_basic/verify.py
@@ -5,40 +5,43 @@ def verify(results):
 
     assert results[0] == MblItem(
         por=LocationItem(name=None),
-        pol=LocationItem(name='SHANGHAI (CN)'),
-        pod=LocationItem(name='LOS ANGELES, CA (US)'),
-        final_dest=LocationItem(name='HOUSTON, TX (US)'),
+        pol=LocationItem(name="SHANGHAI (CN)"),
+        pod=LocationItem(name="LOS ANGELES, CA (US)"),
+        final_dest=LocationItem(name="HOUSTON, TX (US)"),
         eta=None,
-        ata='Mon 17 Jun 2019 00:36',
+        ata="Mon 17 Jun 2019 00:36",
     )
 
     assert results[1] == ContainerItem(
-        container_key='TCNU1868370',
-        container_no='TCNU1868370',
+        container_key="TCNU1868370",
+        container_no="TCNU1868370",
     )
 
     assert results[2] == ContainerStatusItem(
-        container_key='TCNU1868370',
-        local_date_time='Wed 29 May 2019 03:06',
-        description='Empty to shipper',
-        location=LocationItem(name='SHANGHAI'),
-        est_or_actual='A',
+        container_key="TCNU1868370",
+        local_date_time="Wed 29 May 2019 03:06",
+        description="Empty to shipper",
+        location=LocationItem(name="SHANGHAI"),
+        facility="",
+        est_or_actual="A",
     )
 
     assert results[5] == ContainerStatusItem(
-        container_key='TCNU1868370',
-        local_date_time='Mon 17 Jun 2019 00:36',
-        description='Discharged',
-        location=LocationItem(name='LOS ANGELES, CA'),
-        est_or_actual='A',
+        container_key="TCNU1868370",
+        local_date_time="Mon 17 Jun 2019 00:36",
+        description="Discharged",
+        location=LocationItem(name="LOS ANGELES, CA"),
+        facility="FENIX MARINE TERMINAL",
+        est_or_actual="A",
     )
 
     assert results[11] == ContainerStatusItem(
-        container_key='TCNU1868370',
-        local_date_time='Mon 24 Jun 2019 17:15',
-        description='Train arrival for import',
-        location=LocationItem(name='HOUSTON, TX'),
-        est_or_actual='A',
+        container_key="TCNU1868370",
+        local_date_time="Mon 24 Jun 2019 17:15",
+        description="Train arrival for import",
+        location=LocationItem(name="HOUSTON, TX"),
+        facility="UP- ENGLEWOOD",
+        est_or_actual="A",
     )
 
 
@@ -46,43 +49,46 @@ def multi_verify(results):
 
     assert results[0] == MblItem(
         por=LocationItem(name=None),
-        pol=LocationItem(name='SHANGHAI (CN)'),
-        pod=LocationItem(name='LOS ANGELES, CA (US)'),
-        final_dest=LocationItem(name='HOUSTON, TX (US)'),
+        pol=LocationItem(name="SHANGHAI (CN)"),
+        pod=LocationItem(name="LOS ANGELES, CA (US)"),
+        final_dest=LocationItem(name="HOUSTON, TX (US)"),
         eta=None,
-        ata='Mon 17 Jun 2019 00:36',
+        ata="Mon 17 Jun 2019 00:36",
         task_id=1,
     )
 
     assert results[1] == ContainerItem(
-        container_key='TCNU1868370',
-        container_no='TCNU1868370',
+        container_key="TCNU1868370",
+        container_no="TCNU1868370",
         task_id=1,
     )
 
     assert results[2] == ContainerStatusItem(
-        container_key='TCNU1868370',
-        local_date_time='Wed 29 May 2019 03:06',
-        description='Empty to shipper',
-        location=LocationItem(name='SHANGHAI'),
-        est_or_actual='A',
+        container_key="TCNU1868370",
+        local_date_time="Wed 29 May 2019 03:06",
+        description="Empty to shipper",
+        location=LocationItem(name="SHANGHAI"),
+        facility="",
+        est_or_actual="A",
         task_id=1,
     )
 
     assert results[5] == ContainerStatusItem(
-        container_key='TCNU1868370',
-        local_date_time='Mon 17 Jun 2019 00:36',
-        description='Discharged',
-        location=LocationItem(name='LOS ANGELES, CA'),
-        est_or_actual='A',
+        container_key="TCNU1868370",
+        local_date_time="Mon 17 Jun 2019 00:36",
+        description="Discharged",
+        location=LocationItem(name="LOS ANGELES, CA"),
+        facility="FENIX MARINE TERMINAL",
+        est_or_actual="A",
         task_id=1,
     )
 
     assert results[11] == ContainerStatusItem(
-        container_key='TCNU1868370',
-        local_date_time='Mon 24 Jun 2019 17:15',
-        description='Train arrival for import',
-        location=LocationItem(name='HOUSTON, TX'),
-        est_or_actual='A',
+        container_key="TCNU1868370",
+        local_date_time="Mon 24 Jun 2019 17:15",
+        description="Train arrival for import",
+        location=LocationItem(name="HOUSTON, TX"),
+        facility="UP- ENGLEWOOD",
+        est_or_actual="A",
         task_id=1,
     )
