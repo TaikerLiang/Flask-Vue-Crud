68c1584b9a205b8275d9cad82a6763f89622159a
FloraTesng <flora.tseng@gofreight.co>
2022-01-26 17:12:59 +0800

    fix(carrier/cmdu): search all by booking
    [why] search_nos can get correct result searching by booking.
    
    [how] always use search by booking for sending request

--- test/spiders/carrier_anlc/main_info/03_multiple_containers/verify.py
+++ test/spiders/carrier_anlc/main_info/03_multiple_containers/verify.py
@@ -4,7 +4,7 @@ from typing import List
 def verify(results: List):
     results.pop(0)
 
-    expect_url_fmt = 'https://www.anl.com.au/ebusiness/tracking/detail/{container_no}?SearchCriteria=BL&SearchByReference={mbl_no}'
+    expect_url_fmt = "https://www.anl.com.au/ebusiness/tracking/detail/{container_no}?SearchCriteria=Booking&SearchByReference={mbl_no}"
 
     expect_url = expect_url_fmt.format(container_no='TEXU1028151', mbl_no='AWT0143454')
 
@@ -22,10 +22,11 @@ def verify(results: List):
 
     assert results[3].url == expect_url
 
+
 def multi_verify(results: List):
     results.pop(0)
 
-    expect_url_fmt = 'https://www.anl.com.au/ebusiness/tracking/detail/{container_no}?SearchCriteria=BL&SearchByReference={mbl_no}'
+    expect_url_fmt = "https://www.anl.com.au/ebusiness/tracking/detail/{container_no}?SearchCriteria=Booking&SearchByReference={mbl_no}"
 
     expect_url = expect_url_fmt.format(container_no='TEXU1028151', mbl_no='AWT0143454')
 
