eb4cacdf1b6453889bfa17f7bc103eca03b7b7cc
noreason <ken5112840801@gmail.com>
2022-02-08 11:00:40 +0800

    refactor(terminal_e425): Apply pre-commit hook

--- src/crawler/spiders/terminal_apm_e425.py
+++ src/crawler/spiders/terminal_apm_e425.py
@@ -2,6 +2,6 @@ from crawler.core_terminal.apm_share_spider import ApmShareSpider
 
 
 class TerminalApmPESpider(ApmShareSpider):
-    firms_code = 'E425'
-    name = 'terminal_apm_pe'
-    terminal_id = 'cfc387ee-e47e-400a-80c5-85d4316f1af9'
+    firms_code = "E425"
+    name = "terminal_apm_pe"
+    terminal_id = "cfc387ee-e47e-400a-80c5-85d4316f1af9"
--- test/spiders/terminal_apm_e425/container/04_data_not_found/verify.py
+++ test/spiders/terminal_apm_e425/container/04_data_not_found/verify.py
@@ -3,5 +3,5 @@ from crawler.core_terminal.items import InvalidContainerNoItem
 
 def verify(results):
     assert results[0] == InvalidContainerNoItem(
-        container_no='TGCU5024987',
+        container_no="TGCU5024987",
     )
