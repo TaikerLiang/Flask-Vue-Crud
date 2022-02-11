9d38ec602d19255785434769a8519f1d17383795
FloraTesng <flora.tseng@gofreight.co>
2022-02-11 14:11:33 +0800

    fix(terminal_poha_s787): handle task_id info in parse function
    [why] get no result when there are duplecate container nos
    [how] use unique container nos and handle task_id in parse function

--- test/spiders/terminal_poha_s787/container/02_basic_unavailable/verify.py
+++ test/spiders/terminal_poha_s787/container/02_basic_unavailable/verify.py
@@ -7,7 +7,6 @@ def verify(results: List[TerminalItem]):
     assert results[0] == TerminalItem(
         available="Not Available",
         container_no="KOCU4002224",
-        task_id=1,
         carrier="Hyundai America Shipping(HDMU)",
         carrier_release="RELEASED",
         customs_release="RELEASED",
--- test/spiders/terminal_poha_s787/container/e01_invalid_container_no/verify.py
+++ test/spiders/terminal_poha_s787/container/e01_invalid_container_no/verify.py
@@ -7,7 +7,6 @@ from crawler.core_terminal.items import ExportErrorData
 def verify(results: List):
     assert results[0] == ExportErrorData(
         container_no="QQQQQQQQQQQ",
-        task_id=1,
         detail="Data was not found",
         status=TERMINAL_RESULT_STATUS_ERROR,
     )
