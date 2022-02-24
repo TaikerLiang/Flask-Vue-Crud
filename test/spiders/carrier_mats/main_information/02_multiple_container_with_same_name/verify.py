449a9d84dcb5f3fc0c63e8b540ecad928a98630c
Leo Tsao <leo.tsao@gofreight.co>
2022-02-24 13:37:11 +0800

    test(carrier_mats): fix tests
    [why] eta added for MblItem
    [how]

--- test/spiders/carrier_mats/main_information/02_multiple_container_with_same_name/verify.py
+++ test/spiders/carrier_mats/main_information/02_multiple_container_with_same_name/verify.py
@@ -1,18 +1,19 @@
-from crawler.core_carrier.items import MblItem, LocationItem, ContainerItem
+from crawler.core_carrier.items import ContainerItem, LocationItem, MblItem
 from crawler.core_carrier.request_helpers import RequestOption
 
 
 def verify(results):
     assert results[0] == MblItem(
-        por=LocationItem(name='NINGBO'),
-        pol=LocationItem(name='NINGBO'),
-        pod=LocationItem(name='LONG BEACH'),
-        place_of_deliv=LocationItem(name='LONG BEACH'),
+        por=LocationItem(name="NINGBO"),
+        pol=LocationItem(name="NINGBO"),
+        pod=LocationItem(name="LONG BEACH"),
+        place_of_deliv=LocationItem(name="LONG BEACH"),
+        eta=None,
     )
 
     assert results[1] == ContainerItem(
-        container_key='MATU2310910',
-        container_no='MATU2310910',
+        container_key="MATU2310910",
+        container_no="MATU2310910",
     )
 
     assert isinstance(results[2], RequestOption)
