c40b339593b8b34450655049fe482c78d2a01929
noreason <ken5112840801@gmail.com>
2022-03-07 15:22:53 +0800

    test(zimu): refactor
    (cherry picked from commit 70ec074d989d0d6cf0b11c410c8ad7243ac5070e)

---
+++ test/spiders/carrier_zimu/main_info/e01_invalid_mbl_no/verify.py
@@ -0,0 +1,14 @@
+from crawler.core.base import RESULT_STATUS_ERROR, SEARCH_TYPE_MBL
+from crawler.core.items import DataNotFoundItem
+
+
+def verify(results):
+    assert results == [
+        DataNotFoundItem(
+            task_id="1",
+            search_no="ZIMUSNH110567",
+            search_type=SEARCH_TYPE_MBL,
+            status=RESULT_STATUS_ERROR,
+            detail="Data was not found",
+        ),
+    ]
--- test/spiders/carrier_zimu/main_info/test_main_info.py
+++ test/spiders/carrier_zimu/main_info/test_main_info.py
@@ -4,7 +4,8 @@ import pytest
 from scrapy import Request
 from scrapy.http import TextResponse
 
-from crawler.core_carrier.exceptions import CarrierInvalidMblNoError
+from crawler.core.base import SEARCH_TYPE_MBL
+from crawler.core.exceptions import GeneralError
 from crawler.spiders.carrier_zimu import MainInfoRoutingRule
 from test.spiders.carrier_zimu import main_info
 
@@ -30,16 +31,54 @@ def sample_loader(sample_loader):
     ],
 )
 def test_main_info_handle(sub, mbl_no, sample_loader):
+    info_pack = {
+        "task_id": "1",
+        "search_no": mbl_no,
+        "search_type": SEARCH_TYPE_MBL,
+    }
+
     httptext = sample_loader.read_file(sub, "sample.html")
-
-    option = MainInfoRoutingRule.build_request_option(mbl_no=mbl_no)
-
+    option = MainInfoRoutingRule.build_request_option(task_id=info_pack["task_id"], mbl_no=info_pack["search_no"])
     response = TextResponse(
-        url=option.url, body=httptext, encoding="utf-8", request=Request(url=option.url, meta=option.meta,),
+        url=option.url,
+        body=httptext,
+        encoding="utf-8",
+        request=Request(
+            url=option.url,
+            meta=option.meta,
+        ),
     )
 
     rule = MainInfoRoutingRule()
-    results = list(rule.handle_item(response=response))
+    results = list(rule._handle_item(response=response, info_pack=info_pack))
+
+    verify_module = sample_loader.load_sample_module(sub, "verify")
+    verify_module.verify(results=results)
+
+
+@pytest.mark.parametrize(
+    "sub,mbl_no",
+    [
+        ("e01_invalid_mbl_no", "ZIMUSNH110567"),
+    ],
+)
+def test_main_info_handler_is_not_found(sub, mbl_no, sample_loader):
+    httptext = sample_loader.read_file(sub, "sample.html")
+
+    option = MainInfoRoutingRule.build_request_option(task_id="1", mbl_no=mbl_no)
+
+    response = TextResponse(
+        url=option.url,
+        body=httptext,
+        encoding="utf-8",
+        request=Request(
+            url=option.url,
+            meta=option.meta,
+        ),
+    )
+
+    rule = MainInfoRoutingRule()
+    results = list(rule.handle(response=response))
 
     verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.verify(results=results)
@@ -48,17 +87,22 @@ def test_main_info_handle(sub, mbl_no, sample_loader):
 @pytest.mark.parametrize(
     "sub,mbl_no,expect_exception",
     [
-        ("e01_invalid_mbl_no", "ZIMUSNH110567", CarrierInvalidMblNoError),
-        ("e02_invalid_mbl_no_format", "ZIMUORF0946735/1", CarrierInvalidMblNoError),
+        ("e02_invalid_mbl_no_format", "ZIMUORF0946735/1", GeneralError),
     ],
 )
-def test_main_info_handler_mbl_no_error(sub, mbl_no, expect_exception, sample_loader):
+def test_main_info_handler_is_mbl_no_format_error(sub, mbl_no, expect_exception, sample_loader):
     httptext = sample_loader.read_file(sub, "sample.html")
 
-    option = MainInfoRoutingRule.build_request_option(mbl_no=mbl_no)
+    option = MainInfoRoutingRule.build_request_option(task_id="1", mbl_no=mbl_no)
 
     response = TextResponse(
-        url=option.url, body=httptext, encoding="utf-8", request=Request(url=option.url, meta=option.meta,),
+        url=option.url,
+        body=httptext,
+        encoding="utf-8",
+        request=Request(
+            url=option.url,
+            meta=option.meta,
+        ),
     )
 
     rule = MainInfoRoutingRule()
