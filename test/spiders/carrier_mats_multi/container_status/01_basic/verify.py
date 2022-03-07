202557abf7e227583c558bd41484709808da9365
Paul Liang <paul.liang@hardcoretech.co>
2022-03-07 17:10:42 +0800

    fix(carrier/mats): crawler carrier mats was broken
    [why]: the api spec of TimeRoutingRule was different
    [how]: fix to the correct api spec
    
    [FTS-2581]

--- test/spiders/carrier_mats_multi/container_status/01_basic/verify.py
+++ test/spiders/carrier_mats_multi/container_status/01_basic/verify.py
@@ -3,9 +3,9 @@ from crawler.core_carrier.items import ContainerStatusItem, LocationItem
 
 def verify(results):
     assert results[0] == ContainerStatusItem(
-        container_key='MATU2332036',
-        description='RETURNED FROM CONSIGNEE',
-        local_date_time='16/10/2019 14:11:00',
-        location=LocationItem(name='LONG BEACH (CA)'),
-        task_id='1',
+        container_key="MATU2332036",
+        description="RETURNED FROM CONSIGNEE",
+        local_date_time="20/01/2022 08:16:00",
+        location=LocationItem(name="LONG BEACH (CA)"),
+        task_id="1",
     )
