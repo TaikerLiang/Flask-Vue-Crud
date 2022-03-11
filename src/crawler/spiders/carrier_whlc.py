3d538e3a0eb3ffbd06f918d019e3ec9e2547cdc0
noreason <ken5112840801@gmail.com>
2022-03-11 15:50:52 +0800

    feat(whlc): Modify some imports to new-type of imports

--- src/crawler/spiders/carrier_whlc.py
+++ src/crawler/spiders/carrier_whlc.py
@@ -8,7 +8,7 @@ from pyppeteer.errors import ElementHandleError, TimeoutError
 from scrapy import Selector
 from urllib3.exceptions import ReadTimeoutError
 
-from crawler.core.base import (
+from crawler.core.base_new import (
     DUMMY_URL_DICT,
     RESULT_STATUS_ERROR,
     SEARCH_TYPE_BOOKING,
@@ -16,16 +16,20 @@ from crawler.core.base import (
     SEARCH_TYPE_MBL,
 )
 from crawler.core.defines import BaseContentGetter
-from crawler.core.exceptions import FormatError, SuspiciousOperationError, TimeOutError
-from crawler.core.items import DataNotFoundItem
-from crawler.core.proxy import HydraproxyProxyManager, ProxyManager
+from crawler.core.exceptions_new import (
+    FormatError,
+    SuspiciousOperationError,
+    TimeOutError,
+)
+from crawler.core.items_new import DataNotFoundItem
+from crawler.core.proxy_new import HydraproxyProxyManager, ProxyManager
 from crawler.core.pyppeteer import PyppeteerContentGetter
-from crawler.core_carrier.base_spiders import (
+from crawler.core_carrier.base_spiders_new import (
     CARRIER_DEFAULT_SETTINGS,
     DISABLE_DUPLICATE_REQUEST_FILTER,
     BaseCarrierSpider,
 )
-from crawler.core_carrier.items import (
+from crawler.core_carrier.items_new import (
     BaseCarrierItem,
     ContainerItem,
     ContainerStatusItem,
@@ -34,7 +38,7 @@ from crawler.core_carrier.items import (
     MblItem,
     VesselItem,
 )
-from crawler.core_carrier.request_helpers import RequestOption
+from crawler.core_carrier.request_helpers_new import RequestOption
 from crawler.core_carrier.rules import BaseRoutingRule, RequestOptionQueue, RuleManager
 from crawler.extractors.selector_finder import BaseMatchRule, find_selector_from
 from crawler.extractors.table_cell_extractors import BaseTableCellExtractor
@@ -133,13 +137,6 @@ class CarrierWhlcSpider(BaseCarrierSpider):
 
 
 # -------------------------------------------------------------------------------
-
-
-class CarrierIpBlockError(BaseCarrierError):
-    status = CARRIER_RESULT_STATUS_FATAL
-
-    def build_error_data(self):
-        return ExportErrorData(status=self.status, detail="<ip-block-error>")
 
 
 class MblRoutingRule(BaseRoutingRule):
