4dd524f4d192313f50e24252fa3ca8fc223d7d71
noreason <ken5112840801@gmail.com>
2022-03-15 19:20:22 +0800

    test(mccq): Add a test for building special request options

--- test/spiders/carrier_maeu_mccq_safm_multi/mccq/main_info/test_main_info.py
+++ test/spiders/carrier_maeu_mccq_safm_multi/mccq/main_info/test_main_info.py
@@ -6,6 +6,7 @@ from scrapy.http import TextResponse
 
 from crawler.core_carrier.base import SHIPMENT_TYPE_MBL
 from crawler.core_carrier.maeu_mccq_safm_multi_share_spider import MainInfoRoutingRule
+from crawler.core_carrier.request_helpers import RequestOption
 from crawler.spiders.carrier_mccq_multi import CarrierMccqSpider
 from test.spiders.carrier_maeu_mccq_safm_multi.mccq import main_info
 
@@ -49,3 +50,32 @@ def test_main_info_routing_rule(sub, mbl_no, sample_loader):
 
     verify_module = sample_loader.load_sample_module(sub, "verify")
     verify_module.verify(results=results)
+
+
+@pytest.mark.parametrize(
+    "mbl_no,",
+    [
+        ("SEAU216572195"),
+    ],
+)
+def test_build_special_request_option(mbl_no):
+    # arrange
+    expect_option = RequestOption(
+        method=RequestOption.METHOD_GET,
+        rule_name="MAIN_INFO",
+        url=f"https://api.maerskline.com/track/{mbl_no[4:]}?operator=seau",
+        meta={
+            "task_ids": ["1"],
+            "search_nos": [mbl_no],
+            "url_format": "https://api.maerskline.com/track/{search_no}?operator=mcpu",
+            "handle_httpstatus_list": [400, 404],
+        },
+    )
+
+    # action
+    option = MainInfoRoutingRule.build_request_option(
+        search_nos=[mbl_no], task_ids=["1"], url_format=CarrierMccqSpider.base_url_format
+    )
+
+    # assertion
+    assert option == expect_option
