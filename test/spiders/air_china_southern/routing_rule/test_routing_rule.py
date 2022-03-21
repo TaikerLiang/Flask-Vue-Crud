11b03d95f5f5a22f7c395373f016ed7bc3e892b9
colinChen521 <yungyu.chen@gofreight.co>
2022-03-21 16:09:20 +0800

    test(air/china_southern): rearrange import order & add option
    (cherry picked from commit df3bd32a06e893a3ba225e9ffc7b2bac39fabf21)

--- test/spiders/air_china_southern/routing_rule/test_routing_rule.py
+++ test/spiders/air_china_southern/routing_rule/test_routing_rule.py
@@ -4,7 +4,7 @@ import pytest
 from scrapy import Request
 from scrapy.http import TextResponse
 
-from crawler.spiders.air_china_southern import PREFIX, AirInfoRoutingRule
+from crawler.spiders.air_china_southern import AirInfoRoutingRule
 from test.spiders.air_china_southern import routing_rule
 
 
@@ -16,26 +16,24 @@ def sample_loader(sample_loader):
 
 
 @pytest.mark.parametrize(
-    "sub, mawb_no",
+    "sub, task_id, mawb_no",
     [
-        ("01_basic", "66323191"),
-        ("02_data_not_found", "46449060"),
+        ("01_basic", "1", "66323191"),
+        ("02_data_not_found", "1", "46449060"),
     ],
 )
-def test_routing_rule_handle(sub, mawb_no, sample_loader):
+def test_routing_rule_handle(sub, task_id, mawb_no, sample_loader):
     http_text = sample_loader.read_file(sub, "sample.html")
 
-    url = (
-        f"https://tang.csair.com/EN/WebFace/Tang.WebFace.Cargo/AgentAwbBrower.aspx?"
-        f"awbprefix={PREFIX}&awbno={mawb_no}&lan=en-us"
-    )
+    option = AirInfoRoutingRule.build_request_option(task_id=task_id, mawb_no=mawb_no)
+
     response = TextResponse(
-        url=url,
+        url=option.url,
         body=http_text,
         encoding="utf-8",
         request=Request(
-            url=url,
-            meta={"search_no": mawb_no, "task_id": "1"},
+            url=option.url,
+            meta=option.meta,
         ),
     )
 
