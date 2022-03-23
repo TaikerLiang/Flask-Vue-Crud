5b9d317177e83679f9e03426b0c57ccd99302ea1
Leo Tsao <leo.tsao@gofreight.co>
2022-03-23 17:46:12 +0800

    test(terminal_stg): modify test
    [why]
    [how]

--- test/spiders/terminal_stg_y292/container/01_basic/verify.py
+++ test/spiders/terminal_stg_y292/container/01_basic/verify.py
@@ -5,9 +5,7 @@ def verify(results):
     assert results[0] == TerminalItem(
         container_no="OOLU9879270",
         mbl_no="COSU6323480660",
-        ready_for_pick_up="On Vessel",
+        available="On Vessel",
         vessel="CMA CGM A. LINCOLN",
-        last_free_day=None,
         appointment_date="",
-        gate_out_date="",
     )
