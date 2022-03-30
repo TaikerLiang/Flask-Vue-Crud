07646db74b621b4c87c68168f5d4b0552c50f9a7
Leo Tsao <leo.tsao@gofreight.co>
2022-03-30 14:00:51 +0800

    feat(terminal_scspa_lct9): add spider
    [why]
    [how]

---
+++ src/crawler/spiders/terminal_scspa_lct9.py
@@ -0,0 +1,6 @@
+from crawler.core_terminal.scspa_share_spider import ScspaShareSpider
+
+
+class TerminalScspaHughKLeathermanSpider(ScspaShareSpider):
+    firms_code = "LCT9"
+    name = "terminal_scspa_hugh_k_leatherman"
