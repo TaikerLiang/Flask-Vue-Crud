e52487bad37b883eab112a1909ea64407315d6fd
noreason <ken5112840801@gmail.com>
2022-03-30 17:25:17 +0800

    test(gpa_share): Update and Add tests

--- test/spiders/terminal_gpa_l737/container/e01_invalid_container_no/verify.py
+++ test/spiders/terminal_gpa_l737/container/e01_invalid_container_no/verify.py
@@ -1,11 +1,11 @@
-from typing import List, Dict
+from typing import Dict
 
 from crawler.core_terminal.base import TERMINAL_RESULT_STATUS_ERROR
 from crawler.core_terminal.items import ExportErrorData
 
 
-def verify(results: List[Dict]):
-    assert results[0] == ExportErrorData(
+def verify(results: Dict):
+    assert results["QQQQQQQQQQQQ"] == ExportErrorData(
         container_no="QQQQQQQQQQQQ",
         detail="Data was not found",
         status=TERMINAL_RESULT_STATUS_ERROR,
