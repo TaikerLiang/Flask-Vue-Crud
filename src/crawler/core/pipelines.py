ae5d6c846872ffeb5c6f34bba8e7c08694fcc994
noreason <ken5112840801@gmail.com>
2022-04-25 15:37:07 +0800

    fix(core): Correct pipeline syntax error

--- src/crawler/core/pipelines.py
+++ src/crawler/core/pipelines.py
@@ -12,11 +12,11 @@ class BaseItemPipeline:
         url = f'{os.environ.get("EDI_ENGINE_BASE_URL") or ""}{url_suffix}'
         self._edi_client = EdiClientService(url=url, edi_user=user, edi_token=token)
 
-    def send_provider_result_to_edi_client(self, task_id: int, result: Dict):
+    def send_provider_result_to_edi_client(self, task_id: int, item_result: Dict):
         return self._edi_client.send_provider_result_back(
-            task_id=task_id, provider_code="scrapy_cloud_api", item_result=result
+            task_id=task_id, provider_code="scrapy_cloud_api", item_result=item_result
         )
 
     def handle_err_result(self, collector, task_id: int, result: Dict):
         item_result = result if collector.is_default() else collector.build_final_data()
-        return self.send_provider_result_to_edi_client(task_id=task_id, result=item_result)
+        return self.send_provider_result_to_edi_client(task_id=task_id, item_result=item_result)
